1520f08edb05dcc91973b548d0a0fc0c6dfdc648
bernhard <Bernhard.Schmalhofer@gmx.de>
2020-05-23 12:25:27 +0200

    Issue #31: create the user otobo
    And run the processes as otobo.

--- bin/docker/entrypoint.sh
+++ bin/docker/entrypoint.sh
@@ -1,17 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Start up OTOBO Daemon and the webserver
 # Run a watchdog over the Daemon via Cron
 
-# no arguments are handled
+# no command line arguments are handled
 
 # start the Daemon and set up the Cron watchdog
+# assume that we are in /opt/otobo
 mkdir var/tmp
 ./bin/Cron.sh start root
-./bin/otobo.Daemon.pl start
+perl ./bin/otobo.Daemon.pl start
 
 # maintainance jobs
-./bin/otobo.Console.pl Maint::Config::Rebuild
-./bin/otobo.Console.pl Maint::Cache::Delete
+perl ./bin/otobo.Console.pl Maint::Config::Rebuild
+perl ./bin/otobo.Console.pl Maint::Cache::Delete
 
 plackup --server Gazelle --port 5000 bin/psgi-bin/otobo.psgi
