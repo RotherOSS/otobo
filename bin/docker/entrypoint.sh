304f458c448070530670ae2fcf49d597719dde84
bernhard <Bernhard.Schmalhofer@gmx.de>
2020-05-28 09:39:48 +0200

    Issue #40: remove the workaround of calling 'perl bin/otobo.Daemon.pl'
    No longer needed as the Shebank line has been adapted

--- bin/docker/entrypoint.sh
+++ bin/docker/entrypoint.sh
@@ -10,11 +10,16 @@ mkdir -p var/tmp
 
 # set up the cronjobs as they are declared in var/cron
 ./bin/Cron.sh start
-perl ./bin/otobo.Daemon.pl start
+
+# Start the Daemon.
+# The Daemon will exit immediately when SecureMode = 0.
+# But this is OK, as Cron will restart it and it will run when SecureMode = 1.
+./bin/otobo.Daemon.pl start
 
 # maintainance jobs
-# TODO: this might not work when the database is not yet created and configured
+# TODO: decide whether it makes sense to run these jobs on startup
 #perl ./bin/otobo.Console.pl Maint::Config::Rebuild
 #perl ./bin/otobo.Console.pl Maint::Cache::Delete
 
+# Start the webserver
 plackup --server Gazelle -R Kernel,bin/psgi-bin/otobo.psgi --port 5000 bin/psgi-bin/otobo.psgi
