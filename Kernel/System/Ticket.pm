a1c79355e47c78ee709b33ece28455818c037913
Sven <svo@otobo.org>
2020-05-28 08:55:07 +0200

    Add Znuny Copyright to relevant files using Znuny4OTRS-PasswordPolicy and Znuny4OTRS-EscalationSuspend code. Thanks to the Znuny4OTRS team.

--- Kernel/System/Ticket.pm
+++ Kernel/System/Ticket.pm
@@ -3,6 +3,7 @@
 # --
 # Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
 # Copyright (C) 2019-2020 Rother OSS GmbH, https://otobo.de/
+# Copyright (C) 2012-2020 Znuny GmbH, http://znuny.com/
 # --
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
