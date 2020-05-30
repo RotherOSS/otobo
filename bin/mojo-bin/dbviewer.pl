f62a9f0f093e575659a8d77165bd72b634490682
bernhard <Bernhard.Schmalhofer@gmx.de>
2020-05-30 11:02:59 +0200

    Issue #55: Add PDO. Load the Kernel::Config.
    Also remove the not needed stuff in __DATA__.

--- bin/mojo-bin/dbviewer.pl
+++ bin/mojo-bin/dbviewer.pl
@@ -1,28 +1,52 @@
 #!/usr/bin/env perl
 
+=head1 NAME
+
+dbviewer.pl - a Mojolicious::Lite app for viewing the OTOBO database
+
+=head1 SYNOPSIS
+
+    # using the Mojolicious devel webserver
+    morbo bin/morbo-bin/otobo.psgi
+
+=head1 DESCRIPTION
+
+A Mojolicious::Lite application.
+
+Gets the database connection setup from Kernel::Config.
+
+=head1 SEE ALSO
+
+L<https://metacpan.org/pod/Mojolicious::Plugin::DBViewer>
+
+=cut
+
+use strict;
+use warnings;
+use 5.24.0;
+
+# use ../../ as lib location
+use FindBin qw($Bin);
+use lib "$Bin/../..";
+use lib "$Bin/../../Kernel/cpan-lib";
+use lib "$Bin/../../Custom";
+
 # core modules
 
 # CPAN modules
 use Mojolicious::Lite;
 
 # OTOBO modules
+use Kernel::System::ObjectManager;
 
 get '/' => sub {
-  my $c = shift;
-  $c->render(template => 'index');
+    my $c = shift;
+
+    local $Kernel::OM = Kernel::System::ObjectManager->new();
+
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+
+    $c->render(text => 'Hello 🌍!');
 };
 
 app->start;
-__DATA__
-
-@@ index.html.ep
-% layout 'default';
-% title 'Welcome';
-<h1>Welcome to the Mojolicious real-time web framework!</h1>
-
-@@ layouts/default.html.ep
-<!DOCTYPE html>
-<html>
-  <head><title><%= title %></title></head>
-  <body><%= content %></body>
-</html>
