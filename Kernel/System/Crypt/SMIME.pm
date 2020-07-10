cf7bcbd379fcde912a9e947c284bcd2295b4be95
Sven <svo@otobo.org>
2020-07-10 11:41:27 +0200

    Fixed: Wrong SMIME cert is selected in AgentTicketCompose (otrs bug#15075).

--- Kernel/System/Crypt/SMIME.pm
+++ Kernel/System/Crypt/SMIME.pm
@@ -719,9 +719,14 @@ sub _CheckCertificateList {
         if ($Search) {
             ATTRIBUTE:
             for my $Attribute ( sort keys %Attributes ) {
-                if ( $Attributes{$Attribute} =~ m{\Q$Search\E}ixms ) {
-                    $Hit = 1;
-                    last ATTRIBUTE;
+
+                my @Items = split /[,;\s]+/, $Attributes{$Attribute};
+
+                for my $Item (@Items) {
+                    if ( $Item =~ m{^\Q$Search\E$}ixms ) {
+                        $Hit = 1;
+                        last ATTRIBUTE;
+                    }
                 }
             }
         }
@@ -1372,9 +1377,14 @@ sub PrivateSearch {
         if ($Search) {
             ATTRIBUTE:
             for my $Attribute ( sort keys %Attributes ) {
-                if ( $Attributes{$Attribute} =~ m{\Q$Search\E}ixms ) {
-                    $Hit = 1;
-                    last ATTRIBUTE;
+
+                my @Items = split /[,;\s]+/, $Attributes{$Attribute};
+
+                for my $Item (@Items) {
+                    if ( $Item =~ m{^\Q$Search\E$}ixms ) {
+                        $Hit = 1;
+                        last ATTRIBUTE;
+                    }
                 }
             }
         }
