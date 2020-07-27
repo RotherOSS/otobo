17a0930b8230f8e5f20d0e10bf7cadcd90c33a7d
bernhard <Bernhard.Schmalhofer@gmx.de>
2020-07-27 19:12:26 +0200

    Issue #203: remove need for a '## no critic' declaration

--- bin/otobo.CheckSum.pl
+++ bin/otobo.CheckSum.pl
@@ -139,7 +139,7 @@ sub ProcessDirectory {
         next FILE if $File =~ m{css-cache}smx;
 
         # next if not readable
-        open( my $In, '<', $OrigFile ) || die "ERROR: $!";    ## no critic
+        open my $In, '<', $OrigFile or die "ERROR: $!";
 
         my $DigestGenerator = Digest::MD5->new();
         $DigestGenerator->addfile($In);
