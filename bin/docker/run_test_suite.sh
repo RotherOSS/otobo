9acfebca88e6e7a2ca6ca2fb487c1c2e3cba02ad
bernhard <Bernhard.Schmalhofer@gmx.de>
2020-09-24 09:10:56 +0200

    Issue #192: extract the OTOBO version from RELEASE

--- bin/docker/run_test_suite.sh
+++ bin/docker/run_test_suite.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-bin/otobo.Console.pl Dev::UnitTest::Run --verbose >prove_10_1_$(date +'%F-%H%M%S').out 2>&1
+# generate an informative log file name
+otobo_version=$(perl -lne 'print $1 if /VERSION\s*=\s*(\S+)/' < RELEASE)
+time_stamp=$(date +'%F-%H%M%S')
+log_file="prove_${otobo_version}_${time_stamp}.out"
+
+# run the test suite
+bin/otobo.Console.pl Dev::UnitTest::Run --verbose >$log_file 2>&1
