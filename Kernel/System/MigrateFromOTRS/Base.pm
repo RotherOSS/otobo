d7f165e0f28c941ee7d12733405ca8f1fc9385db
bernhard <Bernhard.Schmalhofer@gmx.de>
2020-11-21 15:02:33 +0100

    Issue #644: shorten some columns because of innodb max key length

--- Kernel/System/MigrateFromOTRS/Base.pm
+++ Kernel/System/MigrateFromOTRS/Base.pm
@@ -1330,6 +1330,71 @@ sub DBRenameTables {
     };
 }
 
+# OTOBO VARCHAR attribute shortened to 191 chars, because of InnoDB max key length
+# The values were determined by looking at the patches in scripts/database/otobo-schema.xml.
+sub DBShortenedColumns {
+    return
+        { Table => 'acl', Column => 'name' },
+        { Table => 'acl_sync', Column => 'acl_id' },
+        { Table => 'article_data_mime_send_error', Column => 'message_id' },
+        { Table => 'article_search_index', Column => 'article_key' },
+        { Table => 'article_sender_type', Column => 'name' },
+        { Table => 'auto_response', Column => 'name' },
+        { Table => 'auto_response_type', Column => 'name' },
+        { Table => 'calendar', Column => 'name' },
+        { Table => 'cloud_service_config', Column => 'name' },
+        { Table => 'communication_channel', Column => 'name' },
+        { Table => 'communication_log', Column => 'direction' },
+        { Table => 'communication_log', Column => 'status' },
+        { Table => 'communication_log', Column => 'transport' },
+        { Table => 'communication_log_obj_lookup', Column => 'object_type' },
+        { Table => 'communication_log_object', Column => 'status' },
+        { Table => 'communication_log_object_entry', Column => 'log_key' },
+        { Table => 'customer_company', Column => 'name' },
+        { Table => 'customer_preferences', Column => 'user_id' },
+        { Table => 'customer_user', Column => 'login' },
+        { Table => 'dynamic_field', Column => 'name' },
+        { Table => 'dynamic_field_obj_id_name', Column => 'object_name' },
+        { Table => 'follow_up_possible', Column => 'name' },
+        { Table => 'form_draft', Column => 'action' },
+        { Table => 'generic_agent_jobs', Column => 'job_name' },
+        { Table => 'gi_webservice_config', Column => 'name' },
+        { Table => 'groups_table', Column => 'name' },
+        { Table => 'notification_event', Column => 'name' },
+        { Table => 'notification_event_item', Column => 'event_key' },
+        { Table => 'notification_event_item', Column => 'event_value' },
+        { Table => 'postmaster_filter', Column => 'f_name' },
+        { Table => 'queue', Column => 'name' },
+        { Table => 'roles', Column => 'name' },
+        { Table => 'salutation', Column => 'name' },
+        { Table => 'search_profile', Column => 'login' },
+        { Table => 'search_profile', Column => 'profile_name' },
+        { Table => 'service', Column => 'name' },
+        { Table => 'service_customer_user', Column => 'customer_user_login' },
+        { Table => 'signature', Column => 'name' },
+        { Table => 'sla', Column => 'name' },
+        { Table => 'standard_attachment', Column => 'name' },
+        { Table => 'standard_template', Column => 'name' },
+        { Table => 'sysconfig_default', Column => 'name' },
+        { Table => 'sysconfig_default_version', Column => 'name' },
+        { Table => 'sysconfig_modified', Column => 'name' },
+        { Table => 'sysconfig_modified_version', Column => 'name' },
+        { Table => 'ticket', Column => 'customer_user_id' },
+        { Table => 'ticket', Column => 'title' },
+        { Table => 'ticket_history_type', Column => 'name' },
+        { Table => 'ticket_index', Column => 'queue' },
+        { Table => 'ticket_lock_type', Column => 'name' },
+        { Table => 'ticket_loop_protection', Column => 'sent_to' },
+        { Table => 'ticket_priority', Column => 'name' },
+        { Table => 'ticket_state', Column => 'name' },
+        { Table => 'ticket_state_type', Column => 'name' },
+        { Table => 'ticket_type', Column => 'name' },
+        { Table => 'users', Column => 'login' },
+        { Table => 'valid', Column => 'name' },
+        { Table => 'virtual_fs', Column => 'filename' },
+        { Table => 'virtual_fs_db', Column => 'filename' };
+}
+
 # list of files that need to be copied
 sub CopyFileListfromOTRSToOTOBO {
     my @Files = (
