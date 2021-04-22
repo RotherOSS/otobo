ad35d67db583bee69283a7ed257a49a3d60a1c5b
bernhard <Bernhard.Schmalhofer@gmx.de>
2021-04-22 21:04:29 +0200

    Issue #682: the attribute _TestException was not needed

--- Kernel/System/UnitTest/Selenium.pm
+++ Kernel/System/UnitTest/Selenium.pm
@@ -104,12 +104,6 @@ has _SeleniumTestsConfig => (
     is => 'ro',
 );
 
-# If a test throws an exception, we'll record it here in an attribute so that we can
-# take screenshots.
-has _TestException => (
-    is => 'rw',
-);
-
 # suppress testing events
 has LogExecuteCommandActive => (
     is      => 'rw',
@@ -316,9 +310,8 @@ sub RunTest {
 
     if ( !$CodeSuccess ) {
 
-        # Indicate that during DEMOLISH() the subroutine HandleError() should be called.
-        # HandleError() will create screenshots.
-        $Self->_TestException($@);
+        # HandleError() will create screenshots of the open windows
+        $Self->HandleError($@);
     }
 
     $Context->release();
@@ -955,16 +948,12 @@ and performs some clean-ups.
 sub DEMOLISH {
     my $Self = shift;
 
-    $Self->LogExecuteCommandActive(0);
-
-    if ( $Self->_TestException() ) {
-        $Self->HandleError( $Self->_TestException() );
-    }
-
     return unless $Self->SeleniumTestsActive();
 
+    $Self->LogExecuteCommandActive(0);
+
+    # Cleanup possibly leftover zombie firefox profiles.
     {
-        # Cleanup possibly leftover zombie firefox profiles.
         my @LeftoverFirefoxProfiles = $Kernel::OM->Get('Kernel::System::Main')->DirectoryRead(
             Directory => '/tmp/',
             Filter    => 'anonymous*webdriver-profile',
@@ -975,8 +964,10 @@ sub DEMOLISH {
                 remove_tree($LeftoverFirefoxProfile);
             }
         }
+    }
 
-        # Cleanup all sessions which were created after the selenium test start time.
+    # Cleanup all sessions which were created after the selenium test start time.
+    {
         my $AuthSessionObject = $Kernel::OM->Get('Kernel::System::AuthSession');
 
         my @Sessions = $AuthSessionObject->GetAllSessionIDs();
@@ -996,8 +987,6 @@ sub DEMOLISH {
 
     return;
 }
-
-=head1 DEPRECATED FUNCTIONS
 
 =head2 WaitForjQueryEventBound()
 
