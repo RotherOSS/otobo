5e4e6cfa50cfc225d8053dbd41f22daaff5b67d9
bernhard <Bernhard.Schmalhofer@gmx.de>
2021-05-28 11:55:21 +0200

    Issue #1030: add support for git-*.txt files

--- bin/docker/run_test_suite.sh
+++ bin/docker/run_test_suite.sh
@@ -18,7 +18,12 @@
 # generate an informative log file name
 otobo_version=$(perl -lne 'print $1 if /VERSION\s*=\s*(\S+)/' < RELEASE)
 time_stamp=$(date +'%F-%H%M%S')
-log_file="prove_${otobo_version}_${time_stamp}.out"
+git_branch=$(cat git-branch.txt 2>/dev/null)
+log_file="prove_${otobo_version}_${time_stamp}_${git_branch}.out"
+
+# print out the relevant information about this OTOBO installation
+# Never mind when any of these files are missing
+more RELEASE git-*.txt >$log_file 2>/dev/null
 
 # run the test suite
-bin/otobo.Console.pl Dev::UnitTest::Run --verbose >$log_file 2>&1
+bin/otobo.Console.pl Dev::UnitTest::Run --verbose >>$log_file 2>&1
