0d293c88cd93e9d2cb8dab81b7cce2168831f502
Sven <svo@otobo.org>
2021-11-18 12:05:31 +0100

    Issue #1419: Rework SMIME handling.

--- Kernel/System/PostMaster/NewTicket.pm
+++ Kernel/System/PostMaster/NewTicket.pm
@@ -615,6 +615,18 @@ Message
         return;
     }
 
+    for my $Flag ( qw/Crypted CryptedOK Signed SignedOK/ ) {
+        if ( $GetParam{ $Flag } ) {
+            my $Success = $ArticleObject->ArticleFlagSet(
+                TicketID  => $TicketID,
+                ArticleID => $ArticleID,
+                Key       => $Flag,
+                Value     => $GetParam{ $Flag },
+                UserID    => 1,
+            );
+        }
+    }
+
     $Self->{CommunicationLogObject}->ObjectLookupSet(
         ObjectLogType    => 'Message',
         TargetObjectType => 'Article',
