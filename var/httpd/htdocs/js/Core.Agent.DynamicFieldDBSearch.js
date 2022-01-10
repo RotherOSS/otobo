299c229c2971292443ccf01dacf11699c3c78e99
Stefan Härter <stefan.haerter0@gmail.com>
2022-01-10 11:29:29 +0100

    Issue #1449: Fix Capitalization

--- var/httpd/htdocs/js/Core.Agent.DynamicFieldDBSearch.js
+++ var/httpd/htdocs/js/Core.Agent.DynamicFieldDBSearch.js
@@ -258,8 +258,8 @@ Core.Agent.DynamicFieldDBSearch = (function(TargetNS) {
                     /TicketID=(\d+)/.exec(document.URL);
                     TicketID = RegExp.$1;
 
-                    var liElementId = $Element.parents().closest('li').attr('id');
-                    var activityDialogId = liElementId.match(/^Process_ActivityDialog-([a-f0-9]{32})$/)[1];
+                    var LiElementId = $Element.parents().closest('li').attr('id');
+                    var ActivityDialogId = LiElementId.match(/^Process_ActivityDialog-([a-f0-9]{32})$/)[1];
 
                     // serialize form
                     QueryString = Core.AJAX.SerializeForm($('#'+DynamicFieldName).closest('form'), IgnoreList) + SerializeData(UpdateList);
@@ -267,7 +267,7 @@ Core.Agent.DynamicFieldDBSearch = (function(TargetNS) {
                     QueryString += ";Action="+FrontendInterface;
                     QueryString += ";Term="+encodeURIComponent(Request.term);
                     QueryString += ";MaxResults="+Core.Config.Get('Autocomplete.MaxResultsDisplayed');
-                    QueryString += ";DynamicFieldName="+encodeURIComponent(DynamicFieldName.substring(0, DynamicFieldName.indexOf('_' + activityDialogId)));
+                    QueryString += ";DynamicFieldName="+encodeURIComponent(DynamicFieldName.substring(0, DynamicFieldName.indexOf('_' + ActivityDialogId)));
                     QueryString += ";TicketID="+encodeURIComponent(TicketID);
 
                     URL = Core.Config.Get('Baselink');
