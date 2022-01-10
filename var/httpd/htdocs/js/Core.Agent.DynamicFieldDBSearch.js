2c9f721c39940c30ce0cef6eb52c8d5c0a683554
Stefan Härter <stefan.haerter0@gmail.com>
2022-01-10 10:31:17 +0100

    Issue #1449: Fix another Typo

--- var/httpd/htdocs/js/Core.Agent.DynamicFieldDBSearch.js
+++ var/httpd/htdocs/js/Core.Agent.DynamicFieldDBSearch.js
@@ -259,7 +259,7 @@ Core.Agent.DynamicFieldDBSearch = (function(TargetNS) {
                     TicketID = RegExp.$1;
 
                     var liElementId = $Element.parents().closest('li').attr('id');
-                    var activityDialogID = liElementId.match(/^Process_ActivityDialog-([a-f0-9]{32})$/)[1];
+                    var activityDialogId = liElementId.match(/^Process_ActivityDialog-([a-f0-9]{32})$/)[1];
 
                     // serialize form
                     QueryString = Core.AJAX.SerializeForm($('#'+DynamicFieldName).closest('form'), IgnoreList) + SerializeData(UpdateList);
