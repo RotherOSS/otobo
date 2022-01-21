5068f6d09758274a3b0a2160c465d30a685d4d4b
bernhard <Bernhard.Schmalhofer@gmx.de>
2022-01-21 15:36:55 +0100

    Issue #1471: fix some code comments

--- Kernel/System/Console/Command/Maint/Elasticsearch/Migration.pm
+++ Kernel/System/Console/Command/Maint/Elasticsearch/Migration.pm
@@ -2,7 +2,7 @@
 # OTOBO is a web-based ticketing system for service organisations.
 # --
 # Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
-# Copyright (C) 2019-2021 Rother OSS GmbH, https://otobo.de/
+# Copyright (C) 2019-2022 Rother OSS GmbH, https://otobo.de/
 # --
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -300,7 +300,7 @@ sub MigrateCompanies {
 
         $Count++;
 
-        # create the ticket
+        # create the company in Elasticsearch
         if ( !$Param{ESObject}->CustomerCompanyAdd( CustomerID => $CustomerID ) ) {
             $Errors++;
         }
@@ -419,7 +419,7 @@ sub MigrateCustomerUsers {
 
         $Count++;
 
-        # create the ticket
+        # create the customer user in Elasticsearch
         if ( !$Param{ESObject}->CustomerUserAdd( UserLogin => $CustomerUserID ) ) {
             $Errors++;
         }
@@ -679,7 +679,7 @@ sub MigrateConfigItems {
 
         $Count++;
 
-        # create the ticket
+        # create the config item in Elasticsearch
         if ( !$Param{ESObject}->ConfigItemCreate( ConfigItemID => $ConfigItemID ) ) {
             $Errors++;
         }
