67ce624e9a3f72e922897bd75684035d9c06ebfd
bernhard <Bernhard.Schmalhofer@gmx.de>
2022-04-12 13:40:27 +0200

    Issue #1718: avoid the implicit topic

--- Kernel/System/EmailParser.pm
+++ Kernel/System/EmailParser.pm
@@ -927,11 +927,11 @@ sub GetReferences {
     # get uniq
     my %Checked;
     my @References;
-    for ( reverse @ReferencesAll ) {
-        if ( !$Checked{$_} ) {
-            push @References, $_;
+    for my $Reference ( reverse @ReferencesAll ) {
+        if ( !$Checked{$Reference} ) {
+            push @References, $Reference;
         }
-        $Checked{$_} = 1;
+        $Checked{$Reference} = 1;
     }
     return @References;
 }
