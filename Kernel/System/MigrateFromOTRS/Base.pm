7d03529b33015d90b67780c0fee34986644e3fbb
bernhard <Bernhard.Schmalhofer@gmx.de>
2023-01-16 10:13:04 +0100

    Issue #2077: update the copyright year to 2023
    in the replacement texts of the migration tool

--- Kernel/System/MigrateFromOTRS/Base.pm
+++ Kernel/System/MigrateFromOTRS/Base.pm
@@ -1830,7 +1830,7 @@ sub _ChangeLicenseHeaderRules {
 # OTOBO is a web-based ticketing system for service organisations.
 # --
 ",
-                "# Copyright (C) 2019-2022 Rother OSS GmbH, https://otobo.de/
+                "# Copyright (C) 2019-2023 Rother OSS GmbH, https://otobo.de/
 # --
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -1865,7 +1865,7 @@ sub _ChangeLicenseHeaderRules {
 # OTOBO is a web-based ticketing system for service organisations.
 # --
 ",
-                "# Copyright (C) 2019-2022 Rother OSS GmbH, https://otobo.de/
+                "# Copyright (C) 2019-2023 Rother OSS GmbH, https://otobo.de/
 # --
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -1900,7 +1900,7 @@ sub _ChangeLicenseHeaderRules {
 // OTOBO is a web-based ticketing system for service organisations.
 // --
 ",
-                "// Copyright (C) 2019-2022 Rother OSS GmbH, https://otobo.de/
+                "// Copyright (C) 2019-2023 Rother OSS GmbH, https://otobo.de/
 // --
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -1933,7 +1933,7 @@ sub _ChangeLicenseHeaderRules {
                 "/* OTOBO is a web-based ticketing system for service organisations.
 
 ",
-                "Copyright (C) 2019-2022 Rother OSS GmbH, https://otobo.de/
+                "Copyright (C) 2019-2023 Rother OSS GmbH, https://otobo.de/
 
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
