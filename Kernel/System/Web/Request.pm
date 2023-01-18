dab26378e34f67666fbfc39923065a1229c7ddfe
bernhard <Bernhard.Schmalhofer@gmx.de>
2023-01-18 11:28:17 +0100

    Issue #2075: reinstate the check for XMLHttpRequest.
    This fixes the method IsAJAXRequest().

--- Kernel/System/Web/Request.pm
+++ Kernel/System/Web/Request.pm
@@ -749,9 +749,8 @@ checks if the current request was sent by AJAX
 sub IsAJAXRequest {
     my ($Self) = @_;
 
-    # This method was broken in rel-10_1 such that 0 was always returned.
-    # Let's stay bug compatible for now.
-    return 0;
+    # access request headers via the method http().
+    return ( $Self->{PlackRequest}->header('X-Requested-With') // '' ) eq 'XMLHttpRequest' ? 1 : 0;
 }
 
 =head2 LoadFormDraft()
