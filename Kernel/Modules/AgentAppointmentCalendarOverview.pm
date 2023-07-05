9a6597615128612d48493749026a87b5a84df6b2
Stefan Härter <sha@rother-oss.com>
2023-07-05 14:41:52 +0200

    Issue #2375: Using JSON object True and False methods instead of 1 and empty string.

--- Kernel/Modules/AgentAppointmentCalendarOverview.pm
+++ Kernel/Modules/AgentAppointmentCalendarOverview.pm
@@ -325,7 +325,7 @@ sub Run {
     my $TextDirection = $LayoutObject->{LanguageObject}->{TextDirection} || '';
     $LayoutObject->AddJSData(
         Key   => 'IsRTLLanguage',
-        Value => ( $TextDirection eq 'rtl' ) ? '1' : '',
+        Value => ( $TextDirection eq 'rtl' ) ? $Kernel::OM->Get('Kernel::System::JSON')->True() : $Kernel::OM->Get('Kernel::System::JSON')->False(),
     );
 
     $LayoutObject->AddJSData(
