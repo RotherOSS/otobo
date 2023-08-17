ab0ad97c1a02cf23c7bf18314d6eabd8b4d9fd5e
Sven <svo@otobo.org>
2023-08-17 15:19:15 +0200

    Issue #2255: Add DBAddScriptFieldEventTable to DBUpdateTo11.

---
+++ scripts/DBUpdateTo11_0/DBAddScriptFieldEventTable.pm
@@ -0,0 +1,55 @@
+# --
+# OTOBO is a web-based ticketing system for service organisations.
+# --
+# Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
+# Copyright (C) 2019-2023 Rother OSS GmbH, https://otobo.de/
+# --
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# --
+
+package scripts::DBUpdateTo11_0::DBAddScriptFieldEventTable;
+
+use strict;
+use warnings;
+
+our @ObjectDependencies = (
+    'Kernel::System::DB',
+    'Kernel::System::Log',
+);
+
+=head1 NAME
+
+scripts::DBUpdateTo11_0::DBAddScriptFieldEventTable
+
+=cut
+
+use parent qw(scripts::DBUpdateTo11_0::Base);
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    my @XMLStrings = (
+
+        # New table dynamic_field_script_event
+        '<Table Name="dynamic_field_script_event">
+            <Column Name="id" Required="true" PrimaryKey="true" AutoIncrement="true" Type="INTEGER"/>
+            <Column Name="field_id" Required="true" Type="INTEGER"/>
+            <Column Name="event" Required="true" Type="VARCHAR" Size="191" />
+        </Table>',
+    );
+
+    return if !$Self->ExecuteXMLDBArray(
+        XMLArray => \@XMLStrings,
+    );
+
+    return 1;
+}
+
+1;
