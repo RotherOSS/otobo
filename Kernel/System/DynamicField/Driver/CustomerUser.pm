7482a1b477fc425c9aa03912251fd2b301bcc8f3
Stefan Härter <sha@rother-oss.com>
2023-12-04 13:12:42 +0100

    Issue #2637: Renamed reference field drivers for simplicity.

--- Kernel/System/DynamicField/Driver/CustomerUser.pm
+++ Kernel/System/DynamicField/Driver/CustomerUser.pm
@@ -14,7 +14,7 @@
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 # --
 
-package Kernel::System::DynamicField::Driver::CustomerUserReference;
+package Kernel::System::DynamicField::Driver::CustomerUser;
 
 use v5.24;
 use strict;
@@ -38,7 +38,7 @@ our @ObjectDependencies = (
 
 =head1 NAME
 
-Kernel::System::DynamicField::Driver::CustomerUserReference - backend for the Reference dynamic field
+Kernel::System::DynamicField::Driver::CustomerUser - backend for the Reference dynamic field
 
 =head1 DESCRIPTION
 
