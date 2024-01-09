fcb691a4b30a6d252ab0d35d374d32607d030800
Charly Zauleck <chz@rother-oss.com>
2024-01-09 11:54:27 +0100

    Issue #2313: fixed code policy violations

--- Kernel/System/DynamicField/Driver/Set.pm
+++ Kernel/System/DynamicField/Driver/Set.pm
@@ -115,7 +115,7 @@ my $GetIncludedDynamicFields = sub {
         return $DynamicField;
     };
 
-    my $error = undef;
+    my $Error = undef;
 
     ITEM:
     for my $IncludeItem (@{ $Include }) {
@@ -130,7 +130,7 @@ my $GetIncludedDynamicFields = sub {
                     if ($DynamicField) {
                         push @DynamicFields, $DynamicField;
                     } else {
-                        $error = 1;
+                        $Error = 1;
                         last ITEM;
                     }
                 }
@@ -141,11 +141,11 @@ my $GetIncludedDynamicFields = sub {
             if ($DynamicField) {
                 push @DynamicFields, $DynamicField;
             } else {
-                $error = 1;
+                $Error = 1;
                 last ITEM;
             }
         } else {
-            $error = 1;
+            $Error = 1;
             last ITEM;
         }
     }
@@ -156,7 +156,7 @@ my $GetIncludedDynamicFields = sub {
             Message  => "$DynamicFieldName configured erroneously. 'DF: DynamicFieldName' missing or wrong.",
         );
 
-        return undef;
+        return;
     } else {
         return \@DynamicFields;
     }
@@ -359,7 +359,7 @@ sub EditFieldRender {
 
     if (!$DynamicFields)
              {
-        return undef;
+        return;
     } else {
 
         for my $i (0 .. $#{$DynamicFields}) {
@@ -577,9 +577,8 @@ sub DisplayValueRender {
     my $DynamicFieldName = $Param{DynamicFieldConfig}{Name};
     my $DynamicFields = $GetIncludedDynamicFields->($Include, $DynamicFieldObject, $DynamicFieldName);
 
-    if (!$DynamicFields)
-             {
-        return undef;
+    if (!$DynamicFields) {
+        return;
     } else {
         for my $i (0 .. $#{$DynamicFields}) {
 
@@ -660,9 +659,8 @@ sub ReadableValueRender {
     my $DynamicFieldName = $Param{DynamicFieldConfig}{Name};
     my $DynamicFields = $GetIncludedDynamicFields->($Include, $DynamicFieldObject, $DynamicFieldName);
 
-    if (!$DynamicFields)
-             {
-        return undef;
+    if (!$DynamicFields) {
+        return;
     } else {
         for my $i (0 .. $#{$DynamicFields}) {
 
@@ -764,9 +762,8 @@ sub ValueLookup {
     my $DynamicFieldName = $Param{DynamicFieldConfig}{Name};
     my $DynamicFields = $GetIncludedDynamicFields->($Include, $DynamicFieldObject, $DynamicFieldName);
 
-    if (!$DynamicFields)
-             {
-        return undef;
+    if (!$DynamicFields) {
+        return;
     } else {
         for my $i (0 .. $#{$DynamicFields}) {
 
