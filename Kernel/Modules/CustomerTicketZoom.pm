260a08203da64e140c5c63c8ebe5bd42b600f018
Stefan Härter <sha@rother-oss.com>
2024-01-11 10:12:21 +0100

    Issue #2785: Aligned article dynamic field value behavior with other screens to enable usage of default values.

--- Kernel/Modules/CustomerTicketZoom.pm
+++ Kernel/Modules/CustomerTicketZoom.pm
@@ -2,7 +2,7 @@
 # OTOBO is a web-based ticketing system for service organisations.
 # --
 # Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
-# Copyright (C) 2019-2023 Rother OSS GmbH, https://otobo.de/
+# Copyright (C) 2019-2024 Rother OSS GmbH, https://otobo.de/
 # --
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -1088,15 +1088,20 @@ sub Run {
     $GetParam{NextStateID} ||= $GetParam{StateID} || $Ticket{StateID};
 
     my $CustomerUser = $Self->{UserID};
+
+    # Get values for Ticket fields and use default value for Article fields, if given (this
+    # screen generates a new article, then article fields will be always default value or
+    # empty at the beginning).
     DYNAMICFIELD:
     for my $DynamicFieldConfig ( @{$FollowUpDynamicField} ) {
         next DYNAMICFIELD if !IsHashRefWithData($DynamicFieldConfig);
         if ( $DynamicFieldConfig->{ObjectType} eq 'Ticket' ) {
 
-            # Only get values for Ticket fields (all screens based on AgentTickeActionCommon
-            # generates a new article, then article fields will be always empty at the beginning).
             # Value is stored in the database from Ticket.
             $GetParam{DynamicField}{ 'DynamicField_' . $DynamicFieldConfig->{Name} } = $Ticket{ 'DynamicField_' . $DynamicFieldConfig->{Name} };
+        }
+        elsif ( $DynamicFieldConfig->{ObjectType} eq 'Article' ) {
+            $GetParam{DynamicField}{ 'DynamicField_' . $DynamicFieldConfig->{Name} } = $DynamicFieldConfig->{Config}->{DefaultValue} || '';
         }
     }
 
