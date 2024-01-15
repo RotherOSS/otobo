4030748025f323e67321b057e2caf41ee50b5f63
Sven <svo@otobo.org>
2024-01-15 08:42:52 +0100

    Issue #2814: Fix value set for base entities in DFSet context.

--- Kernel/System/DynamicField/Driver/BaseEntity.pm
+++ Kernel/System/DynamicField/Driver/BaseEntity.pm
@@ -125,8 +125,8 @@ sub ValueSet {
     }
 
     # in MultiValue and Set case, the value is needed as array ref for creating the value structure correctly
-    my $Value = ( $Param{DynamicFieldConfig}{Config}{MultiValue} || $Param{Set} )
-        ? $Param{Value}
+    my $Value = $Param{DynamicFieldConfig}{Config}{MultiValue} ? $Param{Value}
+        : $Param{Set} ? [ map { $_->[0] } $Param{Value}->@* ]
         : $Param{Value}->[0];
 
     my $DBValue = $Self->ValueStructureToDB(
