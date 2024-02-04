e05b7c711231f1ac87e5aa7357350fa56ed4d9c7
bernhard <Bernhard.Schmalhofer@gmx.de>
2024-02-04 10:04:09 +0100

    Issue #2923: Update CPAN::Audit to 20240117.001

--- Kernel/cpan-lib/CPAN/Audit.pm
+++ Kernel/cpan-lib/CPAN/Audit.pm
@@ -14,7 +14,7 @@ use CPAN::Audit::Version;
 use CPAN::Audit::Query;
 use CPAN::Audit::DB;
 
-our $VERSION = '20240103.002';
+our $VERSION = '20240117.001';
 
 sub new {
 	my( $class, %params ) = @_;
