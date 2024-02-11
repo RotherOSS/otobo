2c4037fdf0e0286ad63fb3ef10c341aaa57ae958
bernhard <Bernhard.Schmalhofer@gmx.de>
2024-02-11 11:31:28 +0100

    Issue #2952: upgrade to CPAN::Audit 20240209.001

--- Kernel/cpan-lib/CPAN/Audit.pm
+++ Kernel/cpan-lib/CPAN/Audit.pm
@@ -14,7 +14,7 @@ use CPAN::Audit::Version;
 use CPAN::Audit::Query;
 use CPAN::Audit::DB;
 
-our $VERSION = '20240117.001';
+our $VERSION = '20240209.001';
 
 sub new {
 	my( $class, %params ) = @_;
