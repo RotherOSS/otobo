fa54414bf21558b25a0e8f18185dc0233c4a27d0
Stefan Härter <sha@rother-oss.com>
2024-02-13 14:34:18 +0100

    Issue #2962: Added invalid-checkbox for AdminACL.

--- var/httpd/htdocs/js/Core.Agent.Admin.ACL.js
+++ var/httpd/htdocs/js/Core.Agent.Admin.ACL.js
@@ -57,6 +57,12 @@ Core.Agent.Admin.ACL = (function (TargetNS) {
         if (Core.Config.Get('Subaction') === 'ACLEdit') {
             TargetNS.InitACLEdit();
         }
+
+        // init checkbox to include invalid elements
+        $('input#IncludeInvalid').off('change').on('change', function () {
+            var URL = Core.Config.Get("Baselink") + 'Action=' + Core.Config.Get("Action") + ';IncludeInvalid=' + ( $(this).is(':checked') ? 1 : 0 );
+            window.location.href = URL;
+        });
     };
 
     /**
