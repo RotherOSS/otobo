48699cf9568f04b349aa16970a3ac2d0b13271d1
Stefan Härter <sha@rother-oss.com>
2024-02-14 16:52:26 +0100

    Issue #2962: Added invalid-checkbox for AdminUser.

---
+++ var/httpd/htdocs/js/Core.Agent.Admin.User.js
@@ -0,0 +1,51 @@
+// --
+// OTOBO is a web-based ticketing system for service organisations.
+// --
+// Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
+// Copyright (C) 2019-2024 Rother OSS GmbH, https://otobo.de/
+// --
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later version.
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+// --
+
+"use strict";
+
+var Core = Core || {};
+Core.Agent = Core.Agent || {};
+Core.Agent.Admin = Core.Agent.Admin || {};
+
+ /**
+ * @namespace Core.Agent.Admin.User
+ * @memberof Core.Agent.Admin
+ * @author
+ * @description
+ *      This namespace contains the special module functions for AdminUser.
+ */
+Core.Agent.Admin.User = (function (TargetNS) {
+
+    /**
+     * @name Init
+     * @memberof CCore.Agent.Admin.User
+     * @function
+     * @description
+     *      This function initializes the special module functions
+     */
+    TargetNS.Init = function () {
+
+        // init checkbox to include invalid elements
+        $('input#IncludeInvalid').off('change').on('change', function () {
+            var URL = Core.Config.Get("Baselink") + 'Action=' + Core.Config.Get("Action") + ';IncludeInvalid=' + ( $(this).is(':checked') ? 1 : 0 );
+            window.location.href = URL;
+        });
+    };
+
+    Core.Init.RegisterNamespace(TargetNS, 'APP_MODULE');
+
+    return TargetNS;
+}(Core.Agent.Admin.User || {}));
