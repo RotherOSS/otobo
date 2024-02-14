20a4d6e4249c8623c0ce92b5b0f83010246f91b3
Stefan Härter <sha@rother-oss.com>
2024-02-14 16:07:01 +0100

    Issue #2962: Added invalid-checkbox for AdminTemplate.

--- var/httpd/htdocs/js/Core.Agent.Admin.Template.js
+++ var/httpd/htdocs/js/Core.Agent.Admin.Template.js
@@ -39,6 +39,12 @@ Core.Agent.Admin = Core.Agent.Admin || {};
     TargetNS.Init = function () {
         Core.UI.Table.InitTableFilter($('#Filter'), $('#Templates'));
 
+        // init checkbox to include invalid elements
+        $('input#IncludeInvalid').off('change').on('change', function () {
+            var URL = Core.Config.Get("Baselink") + 'Action=' + Core.Config.Get("Action") + ';IncludeInvalid=' + ( $(this).is(':checked') ? 1 : 0 );
+            window.location.href = URL;
+        });
+
         // delete template
         TargetNS.InitTemplateDelete();
     };
