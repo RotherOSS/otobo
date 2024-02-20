07762054c3688ad93a9db11a43735a6bf7389806
Stefan Härter <sha@rother-oss.com>
2024-02-20 15:48:22 +0100

    Issue #2989: Store preferred sorting in field driver and use it for selections.

--- Kernel/System/DynamicField/Driver/CustomerCompany.pm
+++ Kernel/System/DynamicField/Driver/CustomerCompany.pm
@@ -73,6 +73,9 @@ sub new {
     # Used for declaring CSS classes
     $Self->{FieldCSSClass} = 'DynamicFieldReference';
 
+    # configure sorting for dropdown fields for this field type
+    $Self->{Sort} = 'AlphanumericValue';
+
     # set field behaviors
     $Self->{Behaviors} = {
         'IsACLReducible'               => 0,
