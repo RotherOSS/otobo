943460b354fcf264cdfdafd5882f529fcc40857e
Stefan Härter <sha@rother-oss.com>
2024-02-23 09:45:44 +0100

    Issue #3035: Iterate over keys instead of hash itself when copying config.

--- Kernel/Modules/AdminDynamicFieldDB.pm
+++ Kernel/Modules/AdminDynamicFieldDB.pm
@@ -109,7 +109,7 @@ sub _Add {
                 }
                 elsif ( IsHashRefWithData( $DFDetails->{$ConfigKey} ) ) {
                     my $ConfigContent = $DFDetails->{$ConfigKey};
-                    for my $ContentKey ( $ConfigContent->%* ) {
+                    for my $ContentKey ( keys $ConfigContent->%* ) {
                         $GetParam{$ContentKey} = $ConfigContent->{$ContentKey};
                     }
                 }
--- Kernel/Modules/AdminDynamicFieldSet.pm
+++ Kernel/Modules/AdminDynamicFieldSet.pm
@@ -106,7 +106,7 @@ sub _Add {
                 my $DFDetails = $FieldConfig->{Config};
                 if ( IsHashRefWithData( $DFDetails->{$ConfigKey} ) ) {
                     my $ConfigContent = $DFDetails->{$ConfigKey};
-                    for my $ContentKey ( $ConfigContent->%* ) {
+                    for my $ContentKey ( keys $ConfigContent->%* ) {
                         $GetParam{$ContentKey} = $ConfigContent->{$ContentKey};
                     }
                 }
