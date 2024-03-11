26b6368fad0bf93bba290662301be13a43476c4e
Stefan Härter <sha@rother-oss.com>
2024-03-11 17:00:42 +0100

    Issue #3159: Added new files from package.

---
+++ Kernel/Output/HTML/Notification/CustomerAnnouncementCheck.pm
@@ -0,0 +1,140 @@
+# --
+# OTOBO is a web-based ticketing system for service organisations.
+# --
+# Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
+# Copyright (C) 2019-2024 Rother OSS GmbH, https://otobo.de/
+# --
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# --
+
+package Kernel::Output::HTML::Notification::CustomerAnnouncementCheck;
+
+use parent 'Kernel::Output::HTML::Base';
+
+use strict;
+use warnings;
+
+use Kernel::System::VariableCheck qw(IsHashRefWithData);
+
+our @ObjectDependencies = (
+    'Kernel::Config',
+    'Kernel::Output::HTML::Layout',
+    'Kernel::System::CustomerDashboard::InfoTile',
+    'Kernel::System::HTMLUtils',
+);
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    my $LayoutObject                    = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+    my $CustomerDashboardInfoTileObject = $Kernel::OM->Get('Kernel::System::CustomerDashboard::InfoTile');
+    my $HTMLUtilsObject                 = $Kernel::OM->Get('Kernel::System::HTMLUtils');
+
+    my $Segregation    = $Param{Config}->{Segregation} || '###';
+    my $MarqueeContent = '';
+    my $InfoTiles      = $CustomerDashboardInfoTileObject->InfoTileListGet(
+        UserID => $Self->{UserID},
+    );
+
+    # Check if hash ref is defined
+    if ( IsHashRefWithData($InfoTiles) ) {
+
+        # sort info tiles, sorting order: config order, start date, changed date, created date
+        my @Tiles       = values %{$InfoTiles};
+        my @TilesSorted = $Self->_OrderTiles( Tiles => \@Tiles );
+
+        my $CurrentDate = $Kernel::OM->Create('Kernel::System::DateTime');
+
+        for my $InfoTileRef (@TilesSorted) {
+            my %InfoTile = %{$InfoTileRef};
+            my $StartDate;
+            if ( $InfoTile{StartDateUsed} ) {
+                $StartDate = $Kernel::OM->Create(
+                    'Kernel::System::DateTime',
+                    ObjectParams => {
+                        String => $InfoTile{StartDate}
+                    }
+                );
+            }
+            my $StopDate;
+            if ( $InfoTile{StopDateUsed} ) {
+                $StopDate = $Kernel::OM->Create(
+                    'Kernel::System::DateTime',
+                    ObjectParams => {
+                        String => $InfoTile{StopDate}
+                    }
+                );
+            }
+
+            if (
+                ( ( $StartDate && $CurrentDate->Compare( DateTimeObject => $StartDate ) > 0 ) || !$StartDate )
+                && ( ( $StopDate && $StopDate->Compare( DateTimeObject => $CurrentDate ) > 0 ) || !$StopDate )
+                && $InfoTile{ValidID} eq '1'
+                )
+            {
+
+                if ($MarqueeContent) {
+                    $MarqueeContent .= ' ' . $Segregation . ' ' . $InfoTile{MarqueeContent};
+                }
+                else {
+                    $MarqueeContent = $InfoTile{MarqueeContent};
+                }
+            }
+        }
+    }
+
+    if ($MarqueeContent) {
+        my $Content = $HTMLUtilsObject->DocumentComplete(
+            String            => $MarqueeContent,
+            Charset           => 'utf-8',
+            CustomerInterface => 1,
+        );
+
+        return $LayoutObject->Notify(
+            Priority  => $Param{Config}->{NotifyPriority},
+            Data      => $Content,
+            Link      => $Param{Config}->{UseMarquee} ? '#' : '',    # Workaround to use classes.
+            LinkClass => 'oooMarquee'
+        );
+    }
+}
+
+sub _OrderTiles {
+
+    my ( $Self, %Param ) = @_;
+
+    my @Tiles        = @{ $Param{Tiles} };
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+
+    my $UsedTiles      = $ConfigObject->Get('CustomerDashboard::Tiles');
+    my $TileConfig     = $UsedTiles->{'InfoTile-01'}->{Config};
+    my %TileEntryOrder = %{ $TileConfig->{Order} };
+    my @Result;
+    my @DateSort;
+    for my $TileRef (@Tiles) {
+        my %Tile = %{$TileRef};
+        if ( $TileEntryOrder{ $Tile{Name} } ) {
+            $Tile{Order} = $TileEntryOrder{ $Tile{Name} };
+            push @Result, \%Tile;
+        }
+        else {
+            push @DateSort, \%Tile;
+        }
+    }
+    @Result   = sort { $a->{Order} cmp $b->{Order} } @Result;
+    @DateSort = sort { $b->{StartDate} cmp $a->{StartDate} || $b->{Changed} cmp $a->{Changed} || $b->{Created} cmp $a->{Created} } @DateSort;
+
+    push @Result, @DateSort;
+
+    return @Result;
+
+}
+
+1;
---
+++ var/httpd/htdocs/js/ExtendedCDBInfo.Tile.js
@@ -0,0 +1,121 @@
+// --
+// OTOBO is a web-based ticketing system for service organisations.
+// --
+// Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
+// Copyright (C) 2019-2024 Rother OSS GmbH, https://otobo.de/
+// --
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later version.
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+// --
+
+"use strict";
+
+var ExtendedCDBInfo = ExtendedCDBInfo || {};
+ExtendedCDBInfo.Tile = ExtendedCDBInfo.Tile || {};
+
+/**
+ * @namespace ExtendedCDBInfo.Tile
+ * @memberof ExtendedCDBInfo
+ * @author
+ * @description
+ *      This namespace contains the special functions for the CustomerDashboardInfoTile.
+ */
+ ExtendedCDBInfo.Tile = (function (TargetNS) {
+
+    /**
+     * @private
+     * @name CalculateHeight
+     * @memberof ExtendedCDBInfo.Tile
+     * @function
+     * @param {DOMObject} Iframe - DOM representation of an iframe
+     * @description
+     *      Sets the size of the iframe to the size of its inner html.
+     */
+    function CalculateHeight(Iframe){
+        Iframe = isJQueryObject(Iframe) ? Iframe.get(0) : Iframe;
+
+        setTimeout(function () {
+            var $IframeContent = $(Iframe.contentDocument || Iframe.contentWindow.document),
+                NewHeight = $IframeContent.height();
+
+            if (!NewHeight || isNaN(NewHeight)) {
+                NewHeight = 100;
+            }
+
+            NewHeight = parseInt(NewHeight, 10) + 10;
+            $(Iframe).height(NewHeight + 'px');
+        }, 1000);
+    }
+
+    /**
+     * @private
+     * @name CalculateHeight
+     * @memberof ExtendedCDBInfo.Tile
+     * @function
+     * @param {DOMObject} Iframe - DOM representation of an iframe
+     * @param {Function} [Callback]
+     * @description
+     *      Resizes Iframe to its max inner height and (optionally) calls callback.
+     */
+    function ResizeIframe(Iframe, Callback){
+        Iframe = isJQueryObject(Iframe) ? Iframe.get(0) : Iframe;
+        CalculateHeight(Iframe);
+        if ($.isFunction(Callback)) {
+            Callback();
+        }
+    }
+
+    /**
+     * @private
+     * @name CheckIframe
+     * @memberof ExtendedCDBInfo.Tile
+     * @function
+     * @param {DOMObject} Iframe - DOM representation of an iframe
+     * @param {Function} [Callback]
+     * @description
+     *      This function contains some workarounds for all browsers to get re-size the iframe.
+     * @see http://sonspring.com/journal/jquery-iframe-sizing
+     */
+    function CheckIframe(Iframe, Callback){
+        var Source;
+
+        Iframe = isJQueryObject(Iframe) ? Iframe.get(0) : Iframe;
+
+        if ($.browser.safari || $.browser.opera){
+            $(Iframe).on('load', function() {
+                setTimeout(ResizeIframe, 0, Iframe, Callback);
+            });
+            Source = Iframe.src;
+            Iframe.src = '';
+            Iframe.src = Source;
+        }
+        else {
+            $(Iframe).on('load', function() {
+                ResizeIframe(this, Callback);
+            });
+        }
+    }
+
+    /*
+    * @name Init
+    * @memberof ExtendedCDBInfo.Tile
+    * @function
+    * @description
+    *      This function initializes module functionality.
+    */
+    TargetNS.Init = function () {
+        $('.oooTile_InfoTile iframe').each( function() {
+            CheckIframe($(this));
+        });
+    };
+
+    Core.Init.RegisterNamespace(TargetNS, 'APP_MODULE');
+
+    return TargetNS;
+}(ExtendedCDBInfo.Tile || {}));
