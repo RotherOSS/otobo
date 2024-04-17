aa9bff4ac070aee65eee636bd5993cf071c19ed4
Stefan Härter <sha@rother-oss.com>
2024-04-17 15:07:02 +0200

    Issue #3309: Added basename sanitizing also to FS module of UploadCache.

--- Kernel/System/Web/UploadCache/FS.pm
+++ Kernel/System/Web/UploadCache/FS.pm
@@ -19,6 +19,8 @@ package Kernel::System::Web::UploadCache::FS;
 use strict;
 use warnings;
 
+use File::Basename qw(basename);
+
 our @ObjectDependencies = (
     'Kernel::Config',
     'Kernel::System::Log',
@@ -107,6 +109,8 @@ sub FormIDAddFile {
 
     return if !$Self->_FormIDValidate( $Param{FormID} );
 
+    my $Filename = basename( $Param{Filename} );
+
     $Param{Content} = '' if !defined( $Param{Content} );
 
     # create content id
@@ -143,7 +147,7 @@ sub FormIDAddFile {
     # files must readable for creator
     return if !$MainObject->FileWrite(
         Directory  => $Directory,
-        Filename   => "$Param{Filename}",
+        Filename   => "$Filename",
         Content    => \$Param{Content},
         Mode       => 'binmode',
         Permission => '640',
@@ -151,7 +155,7 @@ sub FormIDAddFile {
     );
     return if !$MainObject->FileWrite(
         Directory  => $Directory,
-        Filename   => "$Param{Filename}.ContentType",
+        Filename   => "$Filename.ContentType",
         Content    => \$Param{ContentType},
         Mode       => 'binmode',
         Permission => '640',
@@ -159,7 +163,7 @@ sub FormIDAddFile {
     );
     return if !$MainObject->FileWrite(
         Directory  => $Directory,
-        Filename   => "$Param{Filename}.ContentID",
+        Filename   => "$Filename.ContentID",
         Content    => \$ContentID,
         Mode       => 'binmode',
         Permission => '640',
@@ -167,7 +171,7 @@ sub FormIDAddFile {
     );
     return if !$MainObject->FileWrite(
         Directory  => $Directory,
-        Filename   => "$Param{Filename}.Disposition",
+        Filename   => "$Filename.Disposition",
         Content    => \$Disposition,
         Mode       => 'binmode',
         Permission => '644',
