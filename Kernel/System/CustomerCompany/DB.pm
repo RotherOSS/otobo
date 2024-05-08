101000764fb069cc3e856b3cec1dae406de36064
Stefan Härter <sha@rother-oss.com>
2024-05-08 12:12:34 +0200

    Issue #3323: Clean up search cache in _CustomerCompanyCacheCleanUp.

--- Kernel/System/CustomerCompany/DB.pm
+++ Kernel/System/CustomerCompany/DB.pm
@@ -986,6 +986,12 @@ sub _CustomerCompanyCacheClear {
     $Self->{CacheObject}->CleanUp(
         Type => $Self->{CacheType} . '_CustomerCompanyList',
     );
+    $Self->{CacheObject}->CleanUp(
+        Type => $Self->{CacheType} . '_CustomerCompanySearchDetail',
+    );
+    $Self->{CacheObject}->CleanUp(
+        Type => $Self->{CacheType} . '_CustomerSearchDetailDynamicFields',
+    );
 
     for my $Function (qw(CustomerCompanyList)) {
         for my $Valid ( 0 .. 1 ) {
