5e7da697ae2852a5929601dd4476b05f3c3e4a06
Stefan Härter <sha@rother-oss.com>
2024-06-03 11:20:30 +0200

    Issue #3186: Further progress.

--- Kernel/System/DynamicField/Driver/BaseReference.pm
+++ Kernel/System/DynamicField/Driver/BaseReference.pm
@@ -1204,7 +1204,11 @@ sub GetFieldState {
     my $DynamicFieldConfig = $Param{DynamicFieldConfig};
 
     return () if !IsArrayRefWithData( $DynamicFieldConfig->{Config}{ReferenceFilterList} );
-    return () if none { $Param{ChangedElements}->{ $_->{EqualsObjectAttribute} // '' } } $DynamicFieldConfig->{Config}{ReferenceFilterList}->@*;
+    return ()
+        if (
+            ( none { $Param{ChangedElements}->{ $_->{EqualsObjectAttribute} // '' } } $DynamicFieldConfig->{Config}{ReferenceFilterList}->@* )
+            && !$Param{Lens}
+        );
 
     my $Value = $Param{GetParam}{DynamicField}{ 'DynamicField_' . $DynamicFieldConfig->{Name} };
 
