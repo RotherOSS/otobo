81e6823b86b1bd97b9d97d4a005fa71739c7a6d4
Stefan Härter <sha@rother-oss.com>
2024-06-10 12:25:59 +0200

    Issue #3447: Pass $Self->{TicketID} to data collection subs in AgentTicketActionCommon.

--- Kernel/Modules/AgentTicketActionCommon.pm
+++ Kernel/Modules/AgentTicketActionCommon.pm
@@ -2,7 +2,7 @@
 # OTOBO is a web-based ticketing system for service organisations.
 # --
 # Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
-# Copyright (C) 2019-2024 Rother OSS GmbH, https://otobo.de/
+# Copyright (C) 2019-2024 Rother OSS GmbH, https://otobo.io/
 # --
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -1542,6 +1542,7 @@ sub Run {
                     $StdFieldValues{ $Check{ $Field->{FieldID} } } = $Field->{Method}->(
                         $Self,
                         %GetParam,
+                        TicketID       => $Self->{TicketID},
                         CustomerUserID => $CustomerUser || '',
                         StateID        => $GetParam{NextStateID},
                     );
@@ -2200,6 +2201,7 @@ sub Run {
                     $StdFieldValues{ $Check{ $Field->{FieldID} } } = $Field->{Method}->(
                         $Self,
                         %GetParam,
+                        TicketID       => $Self->{TicketID},
                         CustomerUserID => $CustomerUser || '',
                         StateID        => $GetParam{NextStateID},
                     );
