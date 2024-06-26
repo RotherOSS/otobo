b9653dee218d13c5b4ebf8a3c5f4c3f8ab3b67b1
Stefan Härter <sha@rother-oss.com>
2024-06-26 14:21:04 +0200

    Issue #3540: Tidying sub ShowTicketStatus.

--- Kernel/Modules/CustomerTicketOverview.pm
+++ Kernel/Modules/CustomerTicketOverview.pm
@@ -15,14 +15,13 @@
 # --
 
 package Kernel::Modules::CustomerTicketOverview;
-## nofilter(TidyAll::Plugin::OTOBO::Perl::DBObject)
 
 use strict;
 use warnings;
 
 our $ObjectManagerDisabled = 1;
 
-use Kernel::System::VariableCheck qw(:all);
+use Kernel::System::VariableCheck qw(IsArrayRefWithData IsHashRefWithData);
 use Kernel::Language              qw(Translatable);
 
 sub new {
@@ -589,18 +588,6 @@ sub Run {
         );
 
         # show tickets
-        #       $Counter = 0;
-        #       for my $TicketID (@ViewableTickets) {
-        #           $Counter++;
-        #           if (
-        #               $Counter >= $StartHit
-        #               && $Counter < ( $PageShown + $StartHit )
-        #               )
-        #           {
-        #               $Self->ShowTicketStatus( TicketID => $TicketID );
-        #           }
-        #       }
-
         my $TicketListObject = $Kernel::OM->Get('Kernel::Output::HTML::TicketOverview::CustomerList');
         $TicketListHTML = $TicketListObject->Run(
             StartHit  => $StartHit,
@@ -656,219 +643,6 @@ sub Run {
     $Output .= $LayoutObject->CustomerFooter();
 
     return $Output;
-}
-
-# ShowTicket
-sub ShowTicketStatus {
-    my ( $Self, %Param ) = @_;
-
-    my $LayoutObject               = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
-    my $TicketObject               = $Kernel::OM->Get('Kernel::System::Ticket');
-    my $ArticleObject              = $Kernel::OM->Get('Kernel::System::Ticket::Article');
-    my $CommunicationChannelObject = $Kernel::OM->Get('Kernel::System::CommunicationChannel');
-    my $TicketID                   = $Param{TicketID} || return;
-
-    # Get last customer article.
-    my @ArticleList = $ArticleObject->ArticleList(
-        TicketID             => $Param{TicketID},
-        IsVisibleForCustomer => 1,
-        OnlyLast             => 1,
-    );
-
-    my %Article;
-    if ( $ArticleList[0] && IsHashRefWithData( $ArticleList[0] ) ) {
-        my $ArticleBackendObject = $ArticleObject->BackendForArticle( %{ $ArticleList[0] } );
-        %Article = $ArticleBackendObject->ArticleGet(
-            TicketID  => $Param{TicketID},
-            ArticleID => $ArticleList[0]->{ArticleID},
-        );
-    }
-
-    # get ticket info
-    my %Ticket = $TicketObject->TicketGet(
-        TicketID      => $TicketID,
-        DynamicFields => 0,
-    );
-
-    my $Subject;
-    my $ConfigObject          = $Kernel::OM->Get('Kernel::Config');
-    my $SmallViewColumnHeader = $ConfigObject->Get('Ticket::Frontend::CustomerTicketOverview')->{ColumnHeader};
-
-    # Check if the last customer subject or ticket title should be shown.
-    if ( $SmallViewColumnHeader eq 'LastCustomerSubject' ) {
-        $Subject = $Article{Subject} || '';
-    }
-    else {
-        $Subject = $Ticket{Title};
-    }
-
-    # Condense down the subject.
-    $Subject = $TicketObject->TicketSubjectClean(
-        TicketNumber => $Ticket{TicketNumber},
-        Subject      => $Subject,
-    );
-
-    # Age design.
-    $Ticket{CustomerAge} = $LayoutObject->CustomerAge(
-        Age   => $Ticket{Age},
-        Space => ' '
-    ) || 0;
-
-    # return ticket information if there is no article
-    if ( !IsHashRefWithData( \%Article ) ) {
-        $Article{State}        = $Ticket{State};
-        $Article{TicketNumber} = $Ticket{TicketNumber};
-        $Article{Body}         = $LayoutObject->{LanguageObject}->Translate('This item has no articles yet.');
-    }
-
-    # customer info (customer name)
-    if ( $Article{CustomerUserID} ) {
-        $Param{CustomerName} = $Kernel::OM->Get('Kernel::System::CustomerUser')->CustomerName(
-            UserLogin => $Article{CustomerUserID},
-        );
-        $Param{CustomerName} = '(' . $Param{CustomerName} . ')' if ( $Param{CustomerName} );
-    }
-
-    # add block
-    $LayoutObject->Block(
-        Name => 'Record',
-        Data => {
-            %Article,
-            %Ticket,
-            Subject => $Subject,
-            %Param,
-        },
-    );
-
-    my $Owner = $ConfigObject->Get('Ticket::Frontend::CustomerTicketOverview')->{Owner};
-    my $Queue = $ConfigObject->Get('Ticket::Frontend::CustomerTicketOverview')->{Queue};
-
-    if ($Owner) {
-        my $OwnerName = $Kernel::OM->Get('Kernel::System::User')->UserName( UserID => $Ticket{OwnerID} );
-        $LayoutObject->Block(
-            Name => 'RecordOwner',
-            Data => {
-                OwnerName => $OwnerName,
-            },
-        );
-    }
-    if ($Queue) {
-        $LayoutObject->Block(
-            Name => 'RecordQueue',
-            Data => {
-                %Ticket,
-            },
-        );
-    }
-
-    # get the dynamic fields for this screen
-    my $DynamicFieldObject = $Kernel::OM->Get('Kernel::System::DynamicField');
-    my $BackendObject      = $Kernel::OM->Get('Kernel::System::DynamicField::Backend');
-
-    # get dynamic field config for frontend module
-    my $DynamicFieldFilter = $ConfigObject->Get("Ticket::Frontend::CustomerTicketOverview")->{DynamicField};
-    my $DynamicField       = $DynamicFieldObject->DynamicFieldListGet(
-        Valid       => 1,
-        ObjectType  => ['Ticket'],
-        FieldFilter => $DynamicFieldFilter || {},
-    );
-
-    # reduce the dynamic fields to only the ones that are desinged for customer interface
-    my @CustomerDynamicFields;
-    DYNAMICFIELD:
-    for my $DynamicFieldConfig ( @{$DynamicField} ) {
-        next DYNAMICFIELD if !IsHashRefWithData($DynamicFieldConfig);
-
-        my $IsCustomerInterfaceCapable = $BackendObject->HasBehavior(
-            DynamicFieldConfig => $DynamicFieldConfig,
-            Behavior           => 'IsCustomerInterfaceCapable',
-        );
-        next DYNAMICFIELD if !$IsCustomerInterfaceCapable;
-
-        push @CustomerDynamicFields, $DynamicFieldConfig;
-    }
-    $DynamicField = \@CustomerDynamicFields;
-
-    # Dynamic fields
-    # cycle trough the activated Dynamic Fields for this screen
-    DYNAMICFIELD:
-    for my $DynamicFieldConfig ( @{$DynamicField} ) {
-        next DYNAMICFIELD if !IsHashRefWithData($DynamicFieldConfig);
-
-        # get field value
-        my $Value = $BackendObject->ValueGet(
-            DynamicFieldConfig => $DynamicFieldConfig,
-            ObjectID           => $Ticket{TicketID},
-        );
-
-        my $ValueStrg = $BackendObject->DisplayValueRender(
-            DynamicFieldConfig => $DynamicFieldConfig,
-            Value              => $Value,
-            ValueMaxChars      => 20,
-            LayoutObject       => $LayoutObject,
-        );
-
-        $LayoutObject->Block(
-            Name => 'RecordDynamicField',
-            Data => {
-                Value => $ValueStrg->{Value},
-                Title => $ValueStrg->{Title},
-            },
-        );
-
-        if ( $ValueStrg->{Link} ) {
-            $LayoutObject->Block(
-                Name => 'RecordDynamicFieldLink',
-                Data => {
-                    Value                       => $ValueStrg->{Value},
-                    Title                       => $ValueStrg->{Title},
-                    Link                        => $ValueStrg->{Link},
-                    $DynamicFieldConfig->{Name} => $ValueStrg->{Title},
-                },
-            );
-        }
-        else {
-            $LayoutObject->Block(
-                Name => 'RecordDynamicFieldPlain',
-                Data => {
-                    Value => $ValueStrg->{Value},
-                    Title => $ValueStrg->{Title},
-                },
-            );
-        }
-
-        # example of dynamic fields order customization
-        $LayoutObject->Block(
-            Name => 'RecordDynamicField' . $DynamicFieldConfig->{Name},
-            Data => {
-                Value => $ValueStrg->{Value},
-                Title => $ValueStrg->{Title},
-            },
-        );
-
-        if ( $ValueStrg->{Link} ) {
-            $LayoutObject->Block(
-                Name => 'RecordDynamicField' . $DynamicFieldConfig->{Name} . 'Link',
-                Data => {
-                    Value                       => $ValueStrg->{Value},
-                    Title                       => $ValueStrg->{Title},
-                    Link                        => $ValueStrg->{Link},
-                    $DynamicFieldConfig->{Name} => $ValueStrg->{Title},
-                },
-            );
-        }
-        else {
-            $LayoutObject->Block(
-                Name => 'RecordDynamicField' . $DynamicFieldConfig->{Name} . 'Plain',
-                Data => {
-                    Value => $ValueStrg->{Value},
-                    Title => $ValueStrg->{Title},
-                },
-            );
-        }
-    }
-
-    return;
 }
 
 1;
