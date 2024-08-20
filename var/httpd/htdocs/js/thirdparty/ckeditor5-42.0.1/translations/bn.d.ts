76768498569d1e40d1665c8dcaf77b38fab60f34
Charly Zauleck <chz@rother-oss.com>
2024-08-20 16:46:28 +0100

    added new ckeditor version

---
+++ var/httpd/htdocs/js/thirdparty/ckeditor5-42.0.1/translations/bn.d.ts
@@ -0,0 +1,8 @@
+/**
+ * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+import type { Translations } from 'ckeditor5';
+declare const translations: Translations;
+export default translations;
