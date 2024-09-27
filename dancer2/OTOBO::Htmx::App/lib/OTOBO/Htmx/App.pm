ec7150031d544485ff95d261cd1b2fe84d88f3b5
bernhard <Bernhard.Schmalhofer@gmx.de>
2024-09-27 16:20:21 +0200

    Actually select data from the database
    in the dummy Dancer2 application.
    Also print out the current PID, for no special reason.

--- dancer2/OTOBO::Htmx::App/lib/OTOBO/Htmx/App.pm
+++ dancer2/OTOBO::Htmx::App/lib/OTOBO/Htmx/App.pm
@@ -1,6 +1,6 @@
 package OTOBO::Htmx::App;
 
-use v5.24;
+use v5.26; # indented heredocs
 use strict;
 use warnings;
 #use namespace::autoclean; # Dancer2 imports symbols
@@ -16,34 +16,60 @@ use Dancer2;
 
 my $h = HTML::Tiny->new( mode => 'html' );
 
-get '/show_table' => sub {
+get '/show_last_logins' => sub {
+    my $DBObject = $Kernel::OM->Get('Kernel::System::DB');
+    my %SessionID2UserFullName = $DBObject->SelectMapping(
+        SQL => <<~'END_SQL',
+            SELECT session_id, data_value
+              FROM sessions
+              WHERE data_key = 'UserFullName'
+            END_SQL
+    );
+    my $LastLogins = $DBObject->SelectAll(
+        SQL => <<~'END_SQL',
+            SELECT session_id, data_value
+              FROM sessions
+              WHERE data_key = 'UserLastLoginTimestamp'
+              ORDER BY data_value DESC
+            END_SQL
+        Limit => 5,
+    );
+
+    my @Rows =
+        map { [ \'td', { id => $_->[0] }, $SessionID2UserFullName{ $_->[0] }, $_->[1] ] }
+        $LastLogins->@*;
+
     return join "\n",
         '',
         $h->div(
             { id => 'breadcrumbs', 'hx-swap-oob' => "true" },
             [
-                \'pre', 'Breadcrumbs updated: ' . localtime,
+                \'pre', "PID=$$ " . 'Breadcrumbs updated at: ' . localtime,
             ],
         ),
         $h->div(
-            { 'hx-get' => "htmx/empty", 'hx-swap' => 'outerHTML' },
+            { class => 'Content', 'hx-get' => "htmx/empty", 'hx-swap' => 'outerHTML' },
             [
                 \'table',
                 [
                     \'tr',
-                    [ \'th', { id => 'sample_head' },  'Key', 'Value' ],
-                    [ \'th', { id => 'sample_col_1' }, 'App', 'OTOBO::Htmx::App' ],
-                    [ \'th', { id => 'sample_col_2' }, 'Time', (scalar localtime) ],
+                    [ \'th', { id => 'sample_head' },  'Full Name', 'Last Login' ],
+                    @Rows,
                 ],
             ],
         );
 };
 
 get '/empty' => sub {
-    return "\n" . $h->div(
-        { 'hx-get' => "htmx/show_table", 'hx-swap' => 'outerHTML' },
-        'Show table'
-    );
+    state $Count = 1;
+    $Count++;
+
+    return join "\n",
+        '',
+        $h->div(
+            { 'hx-get' => "htmx/show_last_logins", 'hx-swap' => 'outerHTML' },
+            "Show table again: PID=$$ Count=$Count"
+        );
 };
 
 true;
