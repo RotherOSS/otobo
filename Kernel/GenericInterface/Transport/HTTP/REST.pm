0aef6b6129d386ed8348098f086e0c7a465d1cc8
Stefan Härter <sha@rother-oss.com>
2025-01-02 14:19:46 +0100

    Issue #3982: Implemented SSL_verify_mode parameter into backend.

--- Kernel/GenericInterface/Transport/HTTP/REST.pm
+++ Kernel/GenericInterface/Transport/HTTP/REST.pm
@@ -2,7 +2,7 @@
 # OTOBO is a web-based ticketing system for service organisations.
 # --
 # Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
-# Copyright (C) 2019-2024 Rother OSS GmbH, https://otobo.io/
+# Copyright (C) 2019-2025 Rother OSS GmbH, https://otobo.io/
 # --
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -633,6 +633,14 @@ sub RequesterPerformRequest {
             )
         {
             $RestClient->getUseragent()->ssl_opts( verify_hostname => 0 );
+        }
+
+        # skip certificate verification
+        if (
+            IsStringWithData( $Config->{SSL}{SSLVerifyMode} )
+            && $Config->{SSL}{SSLVerifyMode} eq 'No'
+            )
+        {
             $RestClient->getUseragent()->ssl_opts( SSL_verify_mode => 0 );
         }
     }
