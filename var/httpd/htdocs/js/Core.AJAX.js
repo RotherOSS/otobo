a341d1ce2954c5eb59cfb548c18afc96ce7c4f45
Mike Thumes <mit@rother-oss.com>
2025-01-07 08:22:14 +0100

    Issue #3973: change the wax Ajax spinner is appended to DOM

--- var/httpd/htdocs/js/Core.AJAX.js
+++ var/httpd/htdocs/js/Core.AJAX.js
@@ -2,7 +2,7 @@
 // OTOBO is a web-based ticketing system for service organisations.
 // --
 // Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
-// Copyright (C) 2019-2024 Rother OSS GmbH, https://otobo.io/
+// Copyright (C) 2019-2025 Rother OSS GmbH, https://otobo.io/
 // --
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -146,7 +146,7 @@ Core.AJAX = (function (TargetNS) {
         // Show or hide the loader
         if (ActiveAJAXCalls[FieldID] > 0) {
             if (!$Loader.length) {
-                $Element.after(LoaderHTML);
+                $Element.parent().append(LoaderHTML);
             }
             else {
                 $Loader.show();
