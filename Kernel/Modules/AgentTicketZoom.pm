4a8224b4ecf9e25605477febc9cc1308b104c7ed
Stefan Härter <sha@rother-oss.com>
2025-01-08 14:48:17 +0100

    Issue #2866: Improved code readability.

--- Kernel/Modules/AgentTicketZoom.pm
+++ Kernel/Modules/AgentTicketZoom.pm
@@ -2,7 +2,7 @@
 # OTOBO is a web-based ticketing system for service organisations.
 # --
 # Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
-# Copyright (C) 2019-2024 Rother OSS GmbH, https://otobo.io/
+# Copyright (C) 2019-2025 Rother OSS GmbH, https://otobo.io/
 # --
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -1548,8 +1548,11 @@ sub MaskAgentZoom {
     elsif ( IsHashRefWithData( $WidgetData{WidgetDynamicField} ) ) {
         DFVALUE:
         for my $FieldName ( keys $WidgetData{WidgetDynamicField}->%* ) {
+
             next DFVALUE unless $Ticket{"DynamicField_$FieldName"};
+
             $ShowWidget = 1;
+
             last DFVALUE;
         }
     }
