19c30b1e6ffb88a4999829b4db3650b09ed30534
Stefan Härter <sha@rother-oss.com>
2025-03-25 11:06:21 +0100

    Issue #4196: Added label translations to EditFieldRender methods.

--- Kernel/System/DynamicField/Driver/BaseText.pm
+++ Kernel/System/DynamicField/Driver/BaseText.pm
@@ -228,7 +228,7 @@ sub EditFieldRender {
     );
 
     my $FieldLabelEscaped = $Param{LayoutObject}->Ascii2Html(
-        Text => $FieldLabel,
+        Text => $Param{LayoutObject}->{LanguageObject}->Translate($FieldLabel),
     );
 
     my %FieldTemplateData = (
