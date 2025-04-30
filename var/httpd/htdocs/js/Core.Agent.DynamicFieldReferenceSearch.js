54f96f8aaeb0525b0cbc4dc534a319a1e5827116
Stefan Härter <sha@rother-oss.com>
2025-04-30 13:39:45 +0200

    Replaced `prevAll` with parent find selector.

--- var/httpd/htdocs/js/Core.Agent.DynamicFieldReferenceSearch.js
+++ var/httpd/htdocs/js/Core.Agent.DynamicFieldReferenceSearch.js
@@ -169,8 +169,8 @@ Core.Agent.DynamicFieldReferenceSearch = (function(TargetNS) {
                 var Visible = false;
 
                 if (!$(this).val()) {
-                    $(this).prevAll('input[type=hidden]').val('');
-                    $('.' + $(this).prevAll('input[type=hidden]').attr('id')).fadeOut('fast', function() {
+                    $(this).parent().find('input[type=hidden]').val('');
+                    $('.' + $(this).parent().find('input[type=hidden]').attr('id')).fadeOut('fast', function() {
 
                         if (!$(this).find('.Reference').hasClass('Hidden')) {
                             Visible = true;
