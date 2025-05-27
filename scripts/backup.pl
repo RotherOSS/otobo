747a9a07ece95ae14e8f1cc49b7d2c33d67c8242
Christopher Bock <christopher@bocki.com>
2025-05-27 05:15:59 +0200

    scripts/backup.pl add zstd to command documentation/help

--- scripts/backup.pl
+++ scripts/backup.pl
@@ -858,8 +858,8 @@ Usage:
 
     # for regular backups, can also be used in a cron job
     otobo> cd /opt/otobo
-    otobo> scripts/backup.pl -d /data_backup_dir [-c gzip|bzip2] [-r DAYS] [-t fullbackup|nofullbackup|dbonly]
-    otobo> scripts/backup.pl --backup-dir /data_backup_dir [--compress gzip|bzip2] [--remove-old-backups DAYS] [--backup-type fullbackup|nofullbackup|dbonly|migratefromotrs]
+    otobo> scripts/backup.pl -d /data_backup_dir [-c gzip|bzip2|zstd] [-r DAYS] [-t fullbackup|nofullbackup|dbonly]
+    otobo> scripts/backup.pl --backup-dir /data_backup_dir [--compress gzip|bzip2|zstd] [--remove-old-backups DAYS] [--backup-type fullbackup|nofullbackup|dbonly|migratefromotrs]
 
     # backups for creating a dump for migrating an OTRS database OTOBO
     otobo> cd /opt/otobo
@@ -871,7 +871,7 @@ Usage:
 Short options:
  [-h]                   - Display help for this command.
  [-d]                   - Directory where the backup files should be placed. Defauls to the current dir.
- [-c]                   - Select the compression method (gzip|bzip2). Defaults to gzip.
+ [-c]                   - Select the compression method (gzip|bzip2|zstd). Defaults to gzip.
  [-r DAYS]              - Remove backups which are more than DAYS days old.
  [-t]                   - Specify which data will be saved (fullbackup|nofullbackup|dbonly|migratefromotrs). Default: fullbackup.
 
