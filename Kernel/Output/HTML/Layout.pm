da092e2ca9b8433468889c96a20cbf425f6c85b9
Mike Thumes <mit@rother-oss.com>
2025-07-09 11:32:38 +0200

    Issue #4393: hide loginform in customer login template if only OIDC Customer Auth modules are active

--- Kernel/Output/HTML/Layout.pm
+++ Kernel/Output/HTML/Layout.pm
@@ -943,7 +943,6 @@ sub Login {
     );
 }
 
-
 sub ChallengeTokenCheck {
     my ( $Self, %Param ) = @_;
 
@@ -4267,6 +4266,8 @@ sub CustomerLogin {
         XLoginHeader => 1,
     );
 
+    $Param{DisableStandardLogin} = $Self->_HasOnlyOIDCAuthModules( LoginType => 'Customer' );
+
     # create & return output
     return $Self->Output(
         TemplateFile => 'CustomerLogin',
@@ -6706,12 +6707,22 @@ sub _HasOnlyOIDCAuthModules {
 
     my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
 
+    my $LoginType = $Param{LoginType} || 'Agent';
+
+    my $LoginModule = $LoginType eq 'Customer'
+        ? 'Customer::AuthModule'
+        : 'AuthModule';
+
+    my $OIDCAuthModule = $LoginType eq 'Customer'
+        ? 'Kernel::System::CustomerAuth::OpenIDConnect'
+        : 'Kernel::System::Auth::OpenIDConnect';
+
     COUNT:
     for my $Count ( '', 1 .. 10 ) {
 
-        my $Module = $ConfigObject->Get("AuthModule$Count");
+        my $Module = $ConfigObject->Get("$LoginModule$Count");
 
-        if ($Module && $Module ne 'Kernel::System::Auth::OpenIDConnect') {
+        if ($Module && $Module ne $OIDCAuthModule ) {
 
             return 0;
         }
