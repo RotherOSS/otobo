0f770f545a7862bb7557a9bf209c75f71f61eac3
Stefan Härter <sha@rother-oss.com>
2025-07-25 12:32:41 +0200

    Issue #4388: Added Modernize to ImportExport dropdowns.

--- Kernel/Modules/AdminImportExport.pm
+++ Kernel/Modules/AdminImportExport.pm
@@ -525,6 +525,7 @@ sub Run {
                 # create form input
                 my $InputString = $LayoutObject->ImportExportFormInputCreate(
                     Item   => $Item,
+                    Class  => 'Modernize',
                     Prefix => 'Object::' . $AttributeRowCounter . '::',
                     Value  => $MappingObjectData->{ $Item->{Key} },
                     ID     => $Item->{Key} . $AttributeRowCounter,
