27428e63ee18a2020b5973608caf828dd16a3623
bernhard <Bernhard.Schmalhofer@gmx.de>
2025-07-28 13:47:33 +0200

    Issue #4528: require Test::Strict in devel:test feature
    also accept the suggestions from CodePolicy

--- bin/otobo.CheckModules.pl
+++ bin/otobo.CheckModules.pl
@@ -70,22 +70,22 @@ use warnings;
 use utf8;
 
 use File::Basename qw(dirname);
-use FindBin qw($RealBin);
+use FindBin        qw($RealBin);
 use lib dirname($RealBin);
 use lib dirname($RealBin) . '/Kernel/cpan-lib';
 use lib dirname($RealBin) . '/Custom';
 
 # core modules
-use Getopt::Long qw(GetOptions);
-use Term::ANSIColor qw(color);
-use Pod::Usage qw(pod2usage);
+use Getopt::Long                   qw(GetOptions);
+use Term::ANSIColor                qw(color);
+use Pod::Usage                     qw(pod2usage);
 use Module::Metadata 1.000031      ();
 use CPAN::Meta::Requirements 2.140 ();
 
 # CPAN modules
 
 # OTOBO modules
-use Kernel::System::Environment ();
+use Kernel::System::Environment   ();
 use Kernel::System::VariableCheck qw(IsHashRefWithData IsArrayRefWithData);
 
 my %InstTypeToCMD = (
@@ -1013,6 +1013,17 @@ my @NeededModules = (
         Comment   => 'a quick compile check',
         InstTypes => {
             aptget => 'libtest-compile-perl',
+            emerge => undef,
+            zypper => undef,
+            ports  => undef,
+        },
+    },
+    {
+        Module    => 'Test::Strict',
+        Features  => ['devel:test'],
+        Comment   => 'check for strictures and warnings',
+        InstTypes => {
+            aptget => undef,
             emerge => undef,
             zypper => undef,
             ports  => undef,
