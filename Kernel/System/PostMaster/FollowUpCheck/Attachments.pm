211b635483839aa8be6f74b8bd3600da0ab5ba3f
bernhard <Bernhard.Schmalhofer@gmx.de>
2025-08-12 12:38:41 +0200

    Issue #4573: enhance code comments

--- Kernel/System/PostMaster/FollowUpCheck/Attachments.pm
+++ Kernel/System/PostMaster/FollowUpCheck/Attachments.pm
@@ -46,6 +46,8 @@ sub Run {
 
     # Ignore all inline parts as these are actually part of the email body.
     my @Attachments = $Self->{ParserObject}->GetAttachments();
+
+    # ignore the parts that are not marked as attachments
     @Attachments = grep { defined $_->{ContentDisposition} && $_->{ContentDisposition} ne 'inline' } @Attachments;
 
     $Self->{CommunicationLogObject}->ObjectLog(
@@ -59,7 +61,8 @@ sub Run {
     for my $Attachment (@Attachments) {
 
         my $Tn = $TicketObject->GetTNByString( $Attachment->{Content} );
-        next ATTACHMENT if !$Tn;
+
+        next ATTACHMENT unless $Tn;
 
         my $TicketID = $TicketObject->TicketCheckNumber( Tn => $Tn );
 
@@ -69,8 +72,7 @@ sub Run {
                 ObjectLogType => 'Message',
                 Priority      => 'Debug',
                 Key           => 'Kernel::System::PostMaster::FollowUpCheck::Attachments',
-                Value         =>
-                    "Found valid TicketNumber '$Tn' (TicketID '$TicketID') in email attachment '$Attachment->{Filename}'.",
+                Value         => "Found valid TicketNumber '$Tn' (TicketID '$TicketID') in email attachment '$Attachment->{Filename}'.",
             );
 
             return $TicketID;
