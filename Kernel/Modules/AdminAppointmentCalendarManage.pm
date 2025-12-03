bc39cb07df5aeca0d35fb104bb11efda427cbf69
Stefan Eisenmann <stefane2741@gmail.com>
2025-12-03 15:41:28 +0100

    Issue #4724: Fixed admin permissions.
    Addded lightadmin functionality.

--- Kernel/Modules/AdminAppointmentCalendarManage.pm
+++ Kernel/Modules/AdminAppointmentCalendarManage.pm
@@ -33,6 +33,10 @@ sub new {
     my $Self = {%Param};
     bless( $Self, $Type );
 
+    if ( !$Param{AccessRw} && $Param{AccessRo} ) {
+        $Self->{LightAdmin} = 1;
+    }
+
     # Certain search parameters for ticket appointments should be stored as scalars, not array refs.
     $Self->{SearchParamScalar} = [
         'MIMEBase_From',
@@ -721,14 +725,22 @@ sub Run {
 sub _Overview {
     my ( $Self, %Param ) = @_;
 
+    my %RequiredPermissions;
+
+    if ( $Self->{LightAdmin} ) {
+        %RequiredPermissions = (
+            UserID     => $Self->{UserID},
+            Permission => 'rw',
+        );
+    }
+
     my $CalendarObject = $Kernel::OM->Get('Kernel::System::Calendar');
 
-    # get all calendars user has RW access to and apply valid state filter
+    # get all calendars user has access to and apply valid state filter
     my @Calendars;
     if ( $Self->{IncludeInvalid} ) {
         push @Calendars, $CalendarObject->CalendarList(
-            UserID     => $Self->{UserID},
-            Permission => 'rw',
+            %RequiredPermissions,
 
             # from CalendarList POD: 0 - All states
             ValidID => 0,
@@ -738,8 +750,7 @@ sub _Overview {
 
         # fetch valid
         push @Calendars, $CalendarObject->CalendarList(
-            UserID     => $Self->{UserID},
-            Permission => 'rw',
+            %RequiredPermissions,
 
             # from CalendarList POD: 1 - All valid
             ValidID => 1,
@@ -747,8 +758,7 @@ sub _Overview {
 
         # fetch invalid-temporarily
         push @Calendars, $CalendarObject->CalendarList(
-            UserID     => $Self->{UserID},
-            Permission => 'rw',
+            %RequiredPermissions,
 
             # from CalendarList POD: 3 - All temporarily invalid
             ValidID => 3,
@@ -857,11 +867,18 @@ sub _Mask {
 sub _GroupSelectionGet {
     my ( $Self, %Param ) = @_;
 
-    # get list of groups where user has RW permissions
-    my %GroupList = $Kernel::OM->Get('Kernel::System::Group')->PermissionUserGet(
-        UserID => $Self->{UserID},
-        Type   => 'rw',
-    );
+    my %GroupList;
+
+    # get all groups that user has access to
+    if ( $Self->{LightAdmin} ) {
+        %GroupList = $Kernel::OM->Get('Kernel::System::Group')->PermissionUserGet(
+            UserID => $Self->{UserID},
+            Type   => 'rw',
+        );
+    }
+    else {
+        %GroupList = $Kernel::OM->Get('Kernel::System::Group')->GroupList( Valid => 1 );
+    }
 
     my $GroupSelection = $Kernel::OM->Get('Kernel::Output::HTML::Layout')->BuildSelection(
         Data        => \%GroupList,
