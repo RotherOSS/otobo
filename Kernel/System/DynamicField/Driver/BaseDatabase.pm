21478b69e3d7cae29dadb296aa817dd74f8d6730
Stefan Härter <sha@rother-oss.com>
2025-12-03 08:34:04 +0100

    Issue #4794: Renamed variables to conform to previous code style.

--- Kernel/System/DynamicField/Driver/BaseDatabase.pm
+++ Kernel/System/DynamicField/Driver/BaseDatabase.pm
@@ -480,25 +480,25 @@ sub EditFieldValueGet {
         )
     {
         if ( $Param{DynamicFieldConfig}{Config}{MultiValue} ) {
-            my @Data = $Param{ParamObject}->GetArray( Param => $FieldName );
+            my @DataAll = $Param{ParamObject}->GetArray( Param => $FieldName );
 
             # delete the template value
-            pop @Data;
+            pop @DataAll;
 
             # prevent things like [""] to pass through
-            my @ValueStorage;
-            my @Values;
+            my @TmpValues;
+            my @Data;
             VALUEITEM:
-            for my $ValueItem (@Data) {
-                push @ValueStorage, $ValueItem;
+            for my $ValueItem (@DataAll) {
+                push @TmpValues, $ValueItem;
 
                 next VALUEITEM if ( !defined $ValueItem || $ValueItem eq '' );
 
-                push @Values, @ValueStorage;
-                @ValueStorage = ();
+                push @Data, @TmpValues;
+                @TmpValues = ();
             }
 
-            $Value = \@Values;
+            $Value = \@Data;
         }
         else {
             $Value = $Param{ParamObject}->GetParam( Param => $FieldName );
