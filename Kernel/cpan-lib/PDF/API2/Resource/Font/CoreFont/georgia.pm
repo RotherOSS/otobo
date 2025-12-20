be3072442abe9df0e205910d86b7e02fd49fcf5d
bernhard <Bernhard.Schmalhofer@gmx.de>
2025-12-20 11:18:37 +0100

    Issue #4977: upgrade PDF::API2 to 2.048

--- Kernel/cpan-lib/PDF/API2/Resource/Font/CoreFont/georgia.pm
+++ Kernel/cpan-lib/PDF/API2/Resource/Font/CoreFont/georgia.pm
@@ -3,7 +3,7 @@ package PDF::API2::Resource::Font::CoreFont::georgia;
 use strict;
 use warnings;
 
-our $VERSION = '2.033'; # VERSION
+our $VERSION = '2.048'; # VERSION
 
 sub data { return {
     'fontname' => 'Georgia',
--- Kernel/cpan-lib/PDF/API2/Resource/XObject/Image/TIFF/File.pm
+++ Kernel/cpan-lib/PDF/API2/Resource/XObject/Image/TIFF/File.pm
@@ -3,7 +3,7 @@ package PDF::API2::Resource::XObject::Image::TIFF::File;
 use strict;
 use warnings;
 
-our $VERSION = '2.033'; # VERSION
+our $VERSION = '2.048'; # VERSION
 
 use IO::File;
 
