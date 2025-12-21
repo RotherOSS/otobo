3d6aaa51d99c574f611a27b94a907efe9578b7d5
bernhard <Bernhard.Schmalhofer@gmx.de>
2025-12-21 16:51:43 +0100

    Issue #4981: update CPANSA::DB to 20251221.001

--- Kernel/System/Environment.pm
+++ Kernel/System/Environment.pm
@@ -334,7 +334,7 @@ sub BundleModulesDeclarationGet {
             'Comment'         => 'database of adbisories used by CPAN::Audit',
             'Module'          => 'CPANSA::DB',
             'Required'        => 1,
-            'VersionRequired' => '== 20251214.001',
+            'VersionRequired' => '== 20251221.001',
         },
         {
             'Comment'         => 'needed by CPAN::Audit',
