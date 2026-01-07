51b5d0433bf0f64a4fa3cade60a238b28ab3a7ae
Stefan Härter <sha@rother-oss.com>
2026-01-07 11:14:45 +0100

    Issue #2152: Accepted CodePolicy suggestion.
    Removed unused variable as well.

--- Kernel/Modules/AdminNotificationEvent.pm
+++ Kernel/Modules/AdminNotificationEvent.pm
@@ -2,7 +2,7 @@
 # OTOBO is a web-based ticketing system for service organisations.
 # --
 # Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
-# Copyright (C) 2019-2025 Rother OSS GmbH, https://otobo.io/
+# Copyright (C) 2019-2026 Rother OSS GmbH, https://otobo.io/
 # --
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -817,7 +817,6 @@ sub Run {
         # challenge token check for write action
         $LayoutObject->ChallengeTokenCheck();
 
-        my $FormID      = $ParamObject->GetParam( Param => 'FormID' ) || '';
         my %UploadStuff = $ParamObject->GetUploadAll(
             Param  => 'FileUpload',
             Source => 'string',
