1b0366442c05ec031f92d21cc92a214b53871cfd
Mike Thumes <mit@rother-oss.com>
2026-01-19 15:06:58 +0100

    Issue #5043: code policy

--- scripts/test/sample/SMIME/make_certs.pl
+++ scripts/test/sample/SMIME/make_certs.pl
@@ -1,10 +1,24 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
+# --
+# OTOBO is a web-based ticketing system for service organisations.
+# --
+# Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
+# Copyright (C) 2019-2026 Rother OSS GmbH, https://otobo.io/
+# --
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# --
 
 use v5.24;
 use utf8;
 use strict;
 use warnings;
-#use lib '/opt/otobo/local/lib/';
 
 use JSON;
 use Path::Class qw(file);
@@ -12,7 +26,6 @@ use Data::Dumper;
 use Time::Piece;
 
 my $ExpireInDays = 365;
-
 
 my %TestCases = (
     1 => 'unittest@example.org',
@@ -30,27 +43,27 @@ my %Passwords = (
 
 my $JSON = {};
 
-foreach my $ID ( 1 .. 3 ) {
+for my $ID ( 1 .. 3 ) {
 
     my $Email = $TestCases{$ID};
 
     print "Email: $Email\n";
 
-    my $KeyOut  = "scripts/test/sample/SMIME/SMIMEPrivateKey-$ID.asc";
-    my $CertOut = "scripts/test/sample/SMIME/SMIMECertificate-$ID.asc";
-    my $Subject = "/C=DE/ST=Bayern/L=Straubing/O=OTOBO/CN=unittest/emailAddress=$Email";
+    my $KeyOut   = "scripts/test/sample/SMIME/SMIMEPrivateKey-$ID.asc";
+    my $CertOut  = "scripts/test/sample/SMIME/SMIMECertificate-$ID.asc";
+    my $Subject  = "/C=DE/ST=Bayern/L=Straubing/O=OTOBO/CN=unittest/emailAddress=$Email";
     my $Password = $Passwords{$ID};
 
     # add SubjectAltNames in special case 3
     my $SubjectAltName = '';
-    if($ID == 3) {
+    if ( $ID == 3 ) {
         $SubjectAltName = ' -addext "subjectAltName = email:unittest4@example.org, email:unittest5@example.org" ';
-         $Email .= ', unittest4@example.org, unittest5@example.org';
+        $Email .= ', unittest4@example.org, unittest5@example.org';
     }
 
     # the openssl command line assembled
     my $Days = $ExpireInDays;
-    if($ID == 4 ) {
+    if ( $ID == 4 ) {
         $Days = 1;
     }
     my $Cmd = "openssl req -x509 -newkey rsa:4096 -passout 'pass:$Password' -keyout $KeyOut -out $CertOut -sha256 -days $Days -subj '$Subject' $SubjectAltName";
@@ -58,45 +71,45 @@ foreach my $ID ( 1 .. 3 ) {
     print "$Cmd\n";
 
     my $Result = system("$Cmd");
-    if($Result != 0) {
+    if ( $Result != 0 ) {
         die "openssl invocation failed!\n";
     }
 
     # fetch data from cert and format for testing
 
-    my $Private     = 'No';
-    my $Type        = "cert";
+    my $Private = 'No';
+    my $Type    = "cert";
 
-    my $Modulus     = qx~openssl x509 -modulus -noout -in $CertOut   | sed 's/^Modulus=//' ~;
-    $Modulus =~ s/\s+$//; # trim whitespace
+    my $Modulus = qx~openssl x509 -modulus -noout -in $CertOut   | sed 's/^Modulus=//' ~;
+    $Modulus =~ s/\s+$//;    # trim whitespace
 
-    my $Subject2    = qx~openssl x509 -subject -noout -in $CertOut   | sed 's/^subject=//' ~;
+    my $Subject2 = qx~openssl x509 -subject -noout -in $CertOut   | sed 's/^subject=//' ~;
     $Subject2 =~ s/\s+$//;
     $Subject2 =~ s/=/= /g;
 
-    my $Issuer      = qx~openssl x509 -issuer -noout -in $CertOut   | sed 's/^issuer=//' ~;
+    my $Issuer = qx~openssl x509 -issuer -noout -in $CertOut   | sed 's/^issuer=//' ~;
     $Issuer =~ s/\s+$//;
     $Issuer =~ s/=/= /g;
 
-    my $Hash        = qx~openssl x509 -hash -noout -in $CertOut ~;
+    my $Hash = qx~openssl x509 -hash -noout -in $CertOut ~;
     $Hash =~ s/\s+$//;
 
-    my $Serial      = qx~openssl x509 -serial -noout -in $CertOut   | sed 's/^serial=//' ~;
+    my $Serial = qx~openssl x509 -serial -noout -in $CertOut   | sed 's/^serial=//' ~;
     $Serial =~ s/\s+$//;
 
     my $Fingerprint = qx~openssl x509 -fingerprint -noout -in $CertOut   | sed 's/^SHA1 Fingerprint=//' ~;
     $Fingerprint =~ s/\s+$//;
 
-    my $EndDate     = qx~openssl x509 -dates -noout -in $CertOut | grep 'notAfter' | sed 's/^notAfter=//' ~;
+    my $EndDate = qx~openssl x509 -dates -noout -in $CertOut | grep 'notAfter' | sed 's/^notAfter=//' ~;
     $EndDate =~ s/\s+$//;
 
-    my $StartDate   = qx~openssl x509 -dates -noout -in $CertOut | grep 'notBefore' | sed 's/^notBefore=//' ~;
+    my $StartDate = qx~openssl x509 -dates -noout -in $CertOut | grep 'notBefore' | sed 's/^notBefore=//' ~;
     $StartDate =~ s/\s+$//;
 
-    my $t = Time::Piece->strptime( $EndDate,"%b %d %H:%M:%S %Y GMT");
+    my $t            = Time::Piece->strptime( $EndDate, "%b %d %H:%M:%S %Y GMT" );
     my $ShortEndDate = $t->strftime("%Y-%m-%d");
 
-    $t = Time::Piece->strptime( $StartDate,"%b %d %H:%M:%S %Y GMT");
+    $t = Time::Piece->strptime( $StartDate, "%b %d %H:%M:%S %Y GMT" );
     my $ShortStartDate = $t->strftime("%Y-%m-%d");
 
     # the whole PEM file as string, for t4est comparisions
@@ -106,7 +119,7 @@ foreach my $ID ( 1 .. 3 ) {
     $JSON->{$ID} = {
         Email          => $Email,
         Modulus        => $Modulus,
-        Subject        => [$Subject, $Subject2],
+        Subject        => [ $Subject, $Subject2 ],
         Issuer         => $Issuer,
         Hash           => $Hash,
         Private        => $Private,
@@ -145,7 +158,9 @@ file("scripts/test/sample/SMIME/smime_test.json")->spew(
 );
 
 # generate the example test .eml encrypted by cert "1"
-system("openssl smime -encrypt -in scripts/test/sample/SMIME/Plain-Test.eml -to 'unittest\@example.org' -from 'unittest\@example.org' -subject 'Unittest data' -out scripts/test/sample/SMIME/SMIME-Test.eml -des3 scripts/test/sample/SMIME/SMIMECertificate-1.asc");
+system(
+    "openssl smime -encrypt -in scripts/test/sample/SMIME/Plain-Test.eml -to 'unittest\@example.org' -from 'unittest\@example.org' -subject 'Unittest data' -out scripts/test/sample/SMIME/SMIME-Test.eml -des3 scripts/test/sample/SMIME/SMIMECertificate-1.asc"
+);
 
 # to test decryption using openssl (passphrase is 'a passphrase')
 # openssl smime -in scripts/test/sample/SMIME/SMIME-Test.eml -inkey scripts/test/sample/SMIME/SMIMEPrivateKey-1.asc -decrypt scripts/test/sample/SMIME/SMIMECertificate-1.asc
