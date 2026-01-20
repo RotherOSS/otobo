ac6848bcb2711f7de42b24d236671e7a8b778389
Mike Thumes <mit@rother-oss.com>
2026-01-20 14:44:22 +0100

    Issue #5055: make SMIME.t more robust, 2nd try

--- scripts/test/sample/SMIME/make_certs.pl
+++ scripts/test/sample/SMIME/make_certs.pl
@@ -2,8 +2,7 @@
 # --
 # OTOBO is a web-based ticketing system for service organisations.
 # --
-# Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
-# Copyright (C) 2019-2026 Rother OSS GmbH, https://otobo.io/
+# Copyright (C) 2026-2026 Rother OSS GmbH, https://otobo.io/
 # --
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -86,10 +85,10 @@ for my $ID ( 1 .. 3 ) {
     $Subject2 =~ s/\s+$//;
     $Subject2 =~ s/=/= /g;
 
-    my $Issuer  = qx~openssl x509 -issuer -noout -in $CertOut   | sed 's/^issuer=//' ~;
-    my $Issuer2 = $Issuer;
-    
+    my $Issuer  = qx~openssl x509 -issuer -noout -in $CertOut   | sed 's/^issuer=//' ~;    
     $Issuer  =~ s/\s+$//;
+    
+    my $Issuer2 = $Issuer;
     $Issuer  =~ s/=/= /g;
     $Issuer2 =~ s/ =/=/g;
 
