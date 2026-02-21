a525506b7d7b3d0f75ebbccc3caca254053bfdf5
bernhard <Bernhard.Schmalhofer@gmx.de>
2026-02-21 10:41:05 +0100

    Update CPANSA::DB 20260220.001
    This version includes the advisory for Crypt::URandom.

--- Kernel/System/Environment.pm
+++ Kernel/System/Environment.pm
@@ -334,7 +334,7 @@ sub BundleModulesDeclarationGet {
             'Comment'         => 'database of adbisories used by CPAN::Audit',
             'Module'          => 'CPANSA::DB',
             'Required'        => 1,
-            'VersionRequired' => '== 20260215.001',
+            'VersionRequired' => '== 20260220.001',
         },
         {
             'Comment'         => 'needed by CPAN::Audit',
