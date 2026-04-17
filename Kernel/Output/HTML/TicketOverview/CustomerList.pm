425a064fc84fc785adc4611591ba854ebbcab963
Stefan Härter <sha@rother-oss.com>
2026-04-17 13:04:35 +0200

    Issue #5329: Removed ValueMaxChars to truncate in the frontend.

--- Kernel/Output/HTML/TicketOverview/CustomerList.pm
+++ Kernel/Output/HTML/TicketOverview/CustomerList.pm
@@ -238,7 +238,6 @@ sub Run {
             my $ValueStrg = $BackendObject->DisplayValueRender(
                 DynamicFieldConfig => $DynamicFieldConfig,
                 Value              => $Value,
-                ValueMaxChars      => 20,
                 LayoutObject       => $LayoutObject,
             );
             next DYNAMICFIELD if ( !defined $ValueStrg->{Value} || $ValueStrg->{Value} eq '' );
