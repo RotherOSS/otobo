0be814a58921fa9f6345db73441f2a63e1b9bc06
bernhard <Bernhard.Schmalhofer@gmx.de>
2026-04-27 13:53:41 +0200

    Issue #5427: add Maint::Translations::Deploy to the update tasks

--- bin/docker/entrypoint.sh
+++ bin/docker/entrypoint.sh
@@ -182,6 +182,7 @@ function do_update_tasks() {
         ($OTOBO_HOME/bin/otobo.Console.pl Maint::Config::Rebuild 2>&1)
         ($OTOBO_HOME/bin/otobo.Console.pl Maint::Cache::Delete 2>&1)
         ($OTOBO_HOME/bin/otobo.Console.pl Maint::Loader::CacheCleanup 2>&1)
+        ($OTOBO_HOME/bin/otobo.Console.pl Maint::Translations::Deploy 2>&1)
         date
         echo "finished $FUNCNAME()"
         echo
