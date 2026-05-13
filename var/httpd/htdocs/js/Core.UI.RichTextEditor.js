8f115a33a5be282d9b6a54fa98a2228806f3be34
chz <chz@rother-oss.com>
2026-05-13 12:14:25 +0100

    Issue 3861: fix some ckeditor mistakes introduced during rebase

--- var/httpd/htdocs/js/Core.UI.RichTextEditor.js
+++ var/httpd/htdocs/js/Core.UI.RichTextEditor.js
@@ -141,9 +141,9 @@ Core.UI.RichTextEditor = (function (TargetNS) {
         var ClassicEditor = CKEditor5Wrapper.ClassicEditor;
         let EnabledPlugins = [];
         for (let pluginName of PluginList) {
-            let Plugin = CKEditor5Plugins[pluginName];
+            let Plugin = CKEditor5Wrapper[pluginName];
             if (Plugin) {
-                EnabledPlugins.push(CKEditor5Plugins[pluginName]);
+                EnabledPlugins.push(CKEditor5Wrapper[pluginName]);
             } else {
                 Core.Exception.ShowError('Couldn\'t find plugin: ' + pluginName, 'JavaScriptError');
             }
@@ -493,17 +493,18 @@ Core.UI.RichTextEditor = (function (TargetNS) {
                     let newEditingAreaSize = newEditorSize - toolbarHeight;
 
                     if (sourceEditingActive) {
-                        $editingArea = $domEditableElement.find('.ck-source-editing-area');
-                        $editingArea.height(newSize);
+                        let $editingArea = $domEditableElement.find('.ck-source-editing-area');
+                        $editingArea.height(newEditingAreaSize);
                         editor.editing.view.forceRender();
                     } else {
+                        let $editingArea = $domEditableElement.find('.ck-content');
                         let borderWidth = parseFloat($editingArea.css("border-top")) 
                                         + parseFloat($editingArea.css("border-bottom"));
-                        newSize -= borderWidth;
+                        newEditingAreaSize -= borderWidth;
                         editor.editing.view.change(writer => {
                             writer.setStyle(
                                 'height',
-                                newSize + 'px',
+                                newEditingAreaSize + 'px',
                                 editor.editing.view.document.getRoot()
                             );
                         });
