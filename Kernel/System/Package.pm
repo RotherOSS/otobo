3670f54e9f33f797dcb087488cfc5292f8d27331
Stefan Härter <sha@rother-oss.com>
2026-05-26 12:49:09 +0200

    Issue #5161: Added package to list of integrated packages.

--- Kernel/System/Package.pm
+++ Kernel/System/Package.pm
@@ -3357,6 +3357,7 @@ sub _GetIntegratedPackages {
             # future releases
             1 => [
                 'CK5-FullWindowMode',
+                'CustomerTicketSearch',
                 'HideShowForAgentTicketCompose',
                 'ImportExportCustomerCompany',
                 'ImportExportStandardObjects',
