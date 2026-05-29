2eb5d6aff5e5eaef223171d5ba876a5cb19b60ec
Stefan Eisenmann <stefane2741@gmail.com>
2026-05-29 16:41:39 +0200

    Issue #5589: Added ProcessEntityID validation to backend modules.

--- Kernel/System/ProcessManagement/DB/ActivityDialog.pm
+++ Kernel/System/ProcessManagement/DB/ActivityDialog.pm
@@ -127,6 +127,31 @@ sub ActivityDialogAdd {
     # get database object
     my $DBObject = $Kernel::OM->Get('Kernel::System::DB');
 
+    # validate ProcessEntityID
+    if ( $Param{ProcessEntityID} ) {
+        return if !$DBObject->Prepare(
+            SQL => '
+                SELECT id
+                FROM pm_process
+                WHERE entity_id = ?',
+            Bind  => [ \$Param{ProcessEntityID} ],
+            Limit => 1,
+        );
+
+        my $ProcessEntityExists;
+        while ( $DBObject->FetchrowArray() ) {
+            $ProcessEntityExists = 1;
+        }
+
+        if ( !$ProcessEntityExists ) {
+            $Kernel::OM->Get('Kernel::System::Log')->Log(
+                Priority => 'error',
+                Message  => "Failed to add process-specific element to '$Param{ProcessEntityID}': No such process!",
+            );
+            return;
+        }
+    }
+
     # check if EntityID already exists
     return if !$DBObject->Prepare(
         SQL => "
