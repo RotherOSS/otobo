82731b0a6a72826f710f80ccf9079a476992e3dd
bernhard <Bernhard.Schmalhofer@gmx.de>
2026-06-01 10:49:12 +0200

    Tidied.
    Added code comments regarding 'Realname' vs. 'RealName'

--- Kernel/System/TemplateGenerator.pm
+++ Kernel/System/TemplateGenerator.pm
@@ -856,7 +856,7 @@ sub AutoResponse {
         );
 
         $AutoResponse{SenderAddress}  = $Address{Name};
-        $AutoResponse{SenderRealname} = $Address{Realname};
+        $AutoResponse{SenderRealname} = $Address{Realname};    # Yes, SystemAddress() does not capitalize the 'n'
     }
 
     # get sender attributes based on queue
@@ -867,7 +867,7 @@ sub AutoResponse {
         );
 
         $AutoResponse{SenderAddress}  = $Address{Email};
-        $AutoResponse{SenderRealname} = $Address{RealName};
+        $AutoResponse{SenderRealname} = $Address{RealName};    # Yes, GetSystemAddress() does capitalize the 'N'
     }
 
     # add urls and verify to be full html document
