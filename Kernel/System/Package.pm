29ddb8f2fcf51e456edf62e6ed0faf4dc376a8d3
Stefan Härter <sha@rother-oss.com>
2026-06-10 14:06:30 +0200

    Issue #3524: Simplified code.

--- Kernel/System/Package.pm
+++ Kernel/System/Package.pm
@@ -5323,8 +5323,6 @@ sub _ConfiguredRepositoryDefinitionGet {
         %RepositoryList = ( %RepositoryList, $Self->PackageOnlineRepositories() );
     }
 
-    return () if !%RepositoryList;
-
     return %RepositoryList;
 }
 
