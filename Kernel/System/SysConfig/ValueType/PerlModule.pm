a5cd939cc8fe5ae20887c541e804a56b8193cc5d
Stefan Härter <sha@rother-oss.com>
2026-06-11 13:17:35 +0200

    Issue #5641: Explicitly do not translate entites and perl modules.
    this affects respective value type entity dropdowns in system
    configuration. The Translation has to be set to 0 explicitly because it
    defaults to 1 if not set.

--- Kernel/System/SysConfig/ValueType/PerlModule.pm
+++ Kernel/System/SysConfig/ValueType/PerlModule.pm
@@ -15,6 +15,7 @@
 # --
 
 package Kernel::System::SysConfig::ValueType::PerlModule;
+
 ## nofilter(TidyAll::Plugin::OTOBO::Perl::LayoutObject)
 
 use strict;
@@ -309,6 +310,7 @@ sub SettingRender {
         Disabled      => $Param{RW} ? 0 : 1,
         SelectedValue => $EffectiveValue,
         Title         => $Param{Name},
+        Translation   => 0,
         OptionTitle   => 1,
         Class         => "$Param{Class} Modernize",
     );
@@ -400,6 +402,7 @@ sub AddItem {
         ID            => $Param{Name} . $Param{IDSuffix},
         SelectedValue => $Param{DefaultItem}->{Content},
         Title         => $Param{Name},
+        Translation   => 0,
         OptionTitle   => 1,
         Class         => "$Param{Class} Modernize Entry",
     );
