a2dc678f3c4bd46aae9d82a5ca829aa4bb81164e
Stefan Härter <sha@rother-oss.com>
2026-06-24 13:32:59 +0200

    Issue #5723: Changed necessary permission to readonly.

--- Kernel/Modules/AgentTicketBulk.pm
+++ Kernel/Modules/AgentTicketBulk.pm
@@ -645,7 +645,7 @@ sub Run {
 
                 # check permissions
                 my $Access = $TicketObject->TicketPermission(
-                    Type     => 'rw',
+                    Type     => 'ro',
                     TicketID => $TicketID,
                     UserID   => $Self->{UserID}
                 );
