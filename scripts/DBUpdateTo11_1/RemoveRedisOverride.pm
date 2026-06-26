636920426fdcc3a5035bb85018893f3b9beabd0e
bernhard <Bernhard.Schmalhofer@gmx.de>
2026-06-26 15:30:21 +0200

    Issue #5728: Change the setting Cache::Module to FileStorable
    when it had been set to Redis in the SysConfig.

--- scripts/DBUpdateTo11_1/RemoveRedisOverride.pm
+++ scripts/DBUpdateTo11_1/RemoveRedisOverride.pm
@@ -17,11 +17,13 @@ package scripts::DBUpdateTo11_1::RemoveRedisOverride;
 
 ## nofilter(TidyAll::Plugin::OTOBO::Perl::Time)
 
-use v5.24;
+use v5.26;
 use strict;
 use warnings;
 use namespace::autoclean;
 use utf8;
+
+use parent qw(scripts::DBUpdateTo11_1::Base);
 
 # core modules
 
@@ -30,6 +32,9 @@ use utf8;
 # OTOBO modules
 
 our @ObjectDependencies = (
+    'Kernel::Config',
+    'Kernel::System::Log',
+    'Kernel::System::SysConfig',
 );
 
 =head1 NAME
@@ -44,8 +49,6 @@ This file overrides the settings in the SysConfig.
 For OTOBO 11.1.x the default caching backend is back to C<FileStorable>.
 
 =cut
-
-use parent qw(scripts::DBUpdateTo11_1::Base);
 
 sub Run {
     my ( $Self, %Param ) = @_;
@@ -68,6 +71,80 @@ sub Run {
         print "\n\n    ERROR: could not tweak Kernel/Config.pm\n";
 
         return 0;
+    }
+
+    my $SysConfigObject = $Kernel::OM->Get('Kernel::System::SysConfig');
+    my $LogObject       = $Kernel::OM->Get('Kernel::System::Log');
+
+    # Tweak the SysConfig if necessary
+    my $Key     = 'Cache::Module';
+    my %Setting = $SysConfigObject->SettingGet(
+        Name => $Key,
+    );
+
+    if (
+        %Setting
+        &&
+        $Setting{IsValid}
+        &&
+        ( $Setting{EffectiveValue} // '' ) eq 'Kernel::System::Cache::Redis'
+        )
+    {
+        print "    changing the SysConfig setting Cache::Module to Kernel::System::Cache::FileStorable\n";
+
+        # nobody else should meddle with the SysConfig
+        my $ExclusiveLockGUID = $SysConfigObject->SettingLock(
+            LockAll => 1,
+            Force   => 1,
+            UserID  => 1,
+        );
+
+        # change the setting
+        my %Result = $SysConfigObject->SettingUpdate(
+            Name              => $Key,
+            IsValid           => 1,
+            EffectiveValue    => 'Kernel::System::Cache::FileStorable',
+            ExclusiveLockGUID => $ExclusiveLockGUID,
+            UserID            => 1,
+        );
+
+        if ( !$Result{Success} ) {
+            $LogObject->Log(
+                Priority => 'warning',
+                Message  => "Could not update setting '$Key'.",
+            );
+
+            return;
+        }
+
+        my $Success = $SysConfigObject->SettingUnlock(
+            UnlockAll => 1,
+        );
+
+        if ( !$Success ) {
+            $LogObject->Log(
+                Priority => 'error',
+                Message  => "Could not unlock settings.",
+            );
+
+            return;
+        }
+
+        my %DeploymentResult = $SysConfigObject->ConfigurationDeploy(
+            AllSettings => 1,
+            Comments    => sprintf( 'UpgradeTo11.1 - %s', __FILE__ ),
+            Force       => 1,
+            UserID      => 1,
+        );
+
+        if ( !$DeploymentResult{Success} ) {
+            $LogObject->Log(
+                Priority => 'error',
+                Message  => "Deployment failed.",
+            );
+
+            return;
+        }
     }
 
     # looking good
