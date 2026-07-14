b15e2271488b2075e09d416f40363faed26843bb
bernhard <Bernhard.Schmalhofer@gmx.de>
2026-07-14 09:54:07 +0200

    Update CPAN::Audit to 20260712.001

--- Kernel/System/Environment.pm
+++ Kernel/System/Environment.pm
@@ -43,7 +43,8 @@ Kernel::System::Environment - collect environment info
 
 =head1 DESCRIPTION
 
-Functions to collect environment info
+Functions to collect environment info.
+Provide a list of Perl modules that are installed in F<Kernel/cpan-lib>.
 
 =head1 PUBLIC INTERFACE
 
@@ -329,7 +330,7 @@ sub BundleModulesDeclarationGet {
             'Comment'         => 'database of adbisories used by CPAN::Audit',
             'Module'          => 'CPANSA::DB',
             'Required'        => 1,
-            'VersionRequired' => '== 20260705.001',
+            'VersionRequired' => '== 20260712.001',
         },
         {
             'Comment'         => 'needed by CPAN::Audit',
