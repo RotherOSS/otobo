70bd90f96f910eea9e7b04afe49df4a60a16004f
bernhard <Bernhard.Schmalhofer@gmx.de>
2026-08-05 17:26:55 +0200

    Issue #5552: use QueueGet() for geting the name of the queue
    Instead of GetSystemAddress().
    This change allow the 'Queue' filter to work for Database Dynamic Fields

--- Kernel/Modules/AgentDynamicFieldDBSearch.pm
+++ Kernel/Modules/AgentDynamicFieldDBSearch.pm
@@ -186,7 +186,7 @@ sub Run {
     my ( $NewQueueID, $From ) = split( /\|\|/, $Param{Dest} );
     $NewQueueID ||= $Param{QueueID} // '';
     if ($NewQueueID) {
-        my %Queue = $QueueObject->GetSystemAddress( QueueID => $NewQueueID );
+        my %Queue = $QueueObject->QueueGet( ID => $NewQueueID );
         $Param{QueueID} = $NewQueueID;
         $Param{Queue}   = $Queue{Name};
     }
