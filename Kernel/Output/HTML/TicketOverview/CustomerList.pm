a5e8ca58b152b109465e1682ce7a07a014bb0e21
Sven <svo@otobo.org>
2026-08-26 16:43:05 +0200

    Ticket categories: Fix undefined value warning if no Order is configured.

--- Kernel/Output/HTML/TicketOverview/CustomerList.pm
+++ Kernel/Output/HTML/TicketOverview/CustomerList.pm
@@ -185,7 +185,7 @@ sub Run {
                 $Conf->{ColorSelection} //= {};
                 my $Color = $Conf->{ColorSelection}{ $Ticket{$CatName} } // $Conf->{ColorDefault};
 
-                push @{ $Categories{ $Conf->{Order} } }, {
+                push @{ $Categories{ $Conf->{Order} // 9999 } }, {
                     Text   => $Text,
                     Color  => $Color,
                     Value  => $Ticket{$CatName},
@@ -244,7 +244,7 @@ sub Run {
                 $Conf->{ColorSelection} //= {};
                 my $Color = $Conf->{ColorSelection}{ $ValueStrg->{Value} } // $Conf->{ColorDefault};
 
-                push @{ $Categories{ $Conf->{Order} } }, {
+                push @{ $Categories{ $Conf->{Order} // 9999 } }, {
                     Text   => $Text,
                     Color  => $Color,
                     Value  => $ValueStrg->{Value},
