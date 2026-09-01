36d9489fda9a8615b0a7d727e4143d4a823fbf73
bernhard <Bernhard.Schmalhofer@gmx.de>
2026-09-01 13:41:45 +0200

    Issue #5998: Handle a message from 'openssl cms'
    The message is "Error reading SMIME Content Info" which seems
    to be emitted when the content isn't signed at all.

--- Kernel/System/PostMaster/Filter/Decrypt.pm
+++ Kernel/System/PostMaster/Filter/Decrypt.pm
@@ -307,7 +307,12 @@ sub _DecryptSMIME {
             }
 
             # not signed at all
-            elsif ( $SignCheck{Message} =~ /^OpenSSL: Error reading S\/MIME message/ ) {
+            elsif (
+                $SignCheck{Message} =~ m/Error reading S\/MIME message/    # openssl smime
+                ||
+                $SignCheck{Message} =~ m/Error reading SMIME Content Info/    # openssl cms
+                )
+            {
                 %SignCheck = ();
             }
 
