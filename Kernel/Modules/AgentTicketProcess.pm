389ee794db2207caaba34c91b1186b504c1c63d5
Stefan Härter <sha@rother-oss.com>
2026-09-04 08:37:30 +0200

    Issue #6086: Implemented pre-filling of form cache for reference fields.
    ACL calculation in lens fields relies upon the cache being present.

--- Kernel/Modules/AgentTicketProcess.pm
+++ Kernel/Modules/AgentTicketProcess.pm
@@ -1847,6 +1847,25 @@ sub _OutputActivityDialog {
             }
         }
 
+        # pre-filling cache for reference field - necessary for ACL calculation of lens fields
+        DYNAMICFIELDNAME:
+        for my $DynamicFieldName ( keys $Self->{DynamicField}->%* ) {
+            my $DynamicFieldConfig = $Self->{DynamicField}{$DynamicFieldName};
+            my $IsReferenceField   = $DynamicFieldBackendObject->HasBehavior(
+                Behavior           => 'IsReferenceField',
+                DynamicFieldConfig => $DynamicFieldConfig,
+            );
+
+            next DYNAMICFIELDNAME unless $IsReferenceField;
+
+            $Kernel::OM->Get('Kernel::System::Web::FormCache')->SetFormData(
+                LayoutObject => $LayoutObject,
+                FormID       => $Self->{FormID},
+                Key          => 'PossibleValues_DynamicField_' . $DynamicFieldConfig->{Name},
+                Value        => $Param{GetParam}{DynamicField}{"DynamicField_$DynamicFieldName"},
+            );
+        }
+
         my $Autoselect     = $ConfigObject->Get('TicketACL::Autoselect') || undef;
         my $LoopProtection = 100;
 
