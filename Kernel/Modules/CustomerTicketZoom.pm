c8d3bce93687dea9ec3c3b43eb5b63c51f60edc4
chz <chz@rother-oss.com>
2026-09-21 09:18:31 +0100

    Issue #6137: fixed typo to get avatar initials for customerticketzoom header

--- Kernel/Modules/CustomerTicketZoom.pm
+++ Kernel/Modules/CustomerTicketZoom.pm
@@ -2490,7 +2490,7 @@ sub _Mask {
         $Avatar = '//www.gravatar.com/avatar/' . md5_hex( lc $CustomerUser{UserEmail} ) . '?s=100&d=' . $DefaultIcon;
     }
     else {
-        $UserInitials = substr( $CustomerUser{UserFirstName}, 0, 1 ) . substr( $CustomerUser{UserLastName}, 0, 1 );
+        $UserInitials = substr( $CustomerUser{UserFirstname}, 0, 1 ) . substr( $CustomerUser{UserLastname}, 0, 1 );
     }
 
     # select the output template
