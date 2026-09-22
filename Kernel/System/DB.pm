4b3176dbd732d47d2ceb4a27a7babc4bfdc5277d
bernhard <Bernhard.Schmalhofer@gmx.de>
2026-09-22 17:57:01 +0200

    Issue #6151: remove comment that refers OTOBO 10.0.0

--- Kernel/System/DB.pm
+++ Kernel/System/DB.pm
@@ -352,13 +352,6 @@ sub Connect {
                 }
             }
 
-            # In OTOBO 10.0.x running with PostgreSQL the flag pg_enable_utf8 was set to 1.
-            # According to https://metacpan.org/pod/DBD::Pg#pg_enable_utf8-(integer)
-            # this is no longer necessary.
-            #if ( $Self->{Backend}->{'DB::Type'} eq 'postgresql' ) {
-            #    $ConnectAttributes{pg_enable_utf8} = 1;
-            #}
-
             $Cache{$CacheKey} = DBIx::Connector->new(
                 $Self->{DSN},
                 $Self->{USER},
